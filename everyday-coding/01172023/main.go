1b35c307aa63ce19b9b1217f2c4f1a8ed3f97093
IMpcuong <trinhtiencuong_t63@hus.edu.vn>
2023-01-17 15:02:58 +0700

    doc: Learning Golang's specs all the way down

--- everyday-coding/01172023/main.go
+++ everyday-coding/01172023/main.go
@@ -6,7 +6,33 @@ import (
 	"unsafe"
 )
 
-// Resource: https://github.com/golang/go/wiki/CommonMistakes
+// Resource:
+//	+ https://github.com/golang/go/wiki/CommonMistakes
+//	+ https://github.com/uber-go/guide/blob/master/style.md
+//	+ https://github.com/golang/go/wiki/CodeReviewComments
+
+// Learning path:
+// 1. https://www.golangtraining.in/lessons/contents.html
+// 2. https://github.com/uber-go/guide/blob/master/style.md
+// 2.1. https://go.dev/doc/effective_go
+// 2.2. https://go.dev/ref/spec
+
+type SomeStruct struct {
+	id   int
+	data string
+}
+
+func NewStruct(id int, data string) *SomeStruct {
+	return &SomeStruct{id, data}
+}
+
+func (s SomeStruct) Read() (int, string) {
+	return s.id, s.data
+}
+
+func (s *SomeStruct) Write(str string) {
+	s.data = str
+}
 
 func main() {
 	out := make([]*int, 10)
@@ -58,4 +84,24 @@ func main() {
 			fmt.Println(i, v) // NOTE: Without executing closure as goroutine, the code run as expected.
 		}()
 	}
+
+	newS := NewStruct(2, "Hey")
+	valStruct := map[int]*SomeStruct{1: {1, "Dude"}, 2: newS}
+	valStruct[1].Write("Neh!!!")
+
+	// NOTE: As with method calls, a reference to a non-interface method with a value receiver
+	//	using an adressable value will automatically take the address of that value.
+	//	Document:
+	//		+ https://go.dev/ref/spec#Method_values
+	// 		+ https://www.golangtraining.in/lessons/pointers/addressable-values.html
+	//		+ https://go.dev/doc/effective_go#pointers_vs_values
+
+	indirective_func := (*valStruct[1]).Write // Equals: `val[1].Write` --> `SomeStruct` is an addressable type.
+	indirective_func("Dude again")
+
+	// NOTE: Methods with value receivers can be called on pointers as well as values.
+	//	Methods with pointer receivers can only be called on pointers or addressable values.
+	fmt.Println(valStruct[1].Read()) // Equivalent (semantically): `(*SomeStruct).Read()`.
+	fmt.Println(valStruct[2].Read())
+	fmt.Printf("%#v\n", valStruct)
 }
