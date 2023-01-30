9001b9433dee613ddf5d80ee98275e9d2c18183b
IMpcuong <trinhtiencuong_t63@hus.edu.vn>
2023-01-30 10:44:28 +0700

    learn(clang): Macro function, static kwrd, array-ptr, typedef-struct
    * Implements a relatively complicated code inside a macro rule, the
      recommendation is using a `inline` or `static` function in header file
      instead. The result is as same efficiency as a macro rule.
    
    * Static type equivalent with lifetime annotation in Rust to expand the
      existence of a variable outside of its function's scope (callable
      after function end).
    
    * Array decays to pointers in C: array assignment statement is only
      accepted its pointer (reference) to be reassigned by another array,
      sequential elements can be addressed by using pointer incremental
      technique (1 pointer := 2^32 (32 bit) || 2^64 (64 bit) depends on
      architecture; 4 bytes || 8 bytes).
    
      Return an array-type by selecting it from its wrapped struct, this is
      one of some work-around methods when C didn't allow this return type
      directively.
    
    * `typedef` := type alias in Golang; `typedef struct X {} X;` := giving
      the current struct an alias name.

--- clang-the-hardway/array_as_return_type.c
+++ clang-the-hardway/array_as_return_type.c
@@ -1,31 +1,116 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-// Source from: https://stackoverflow.com/questions/14297169/how-to-make-an-array-return-type-from-c-function
+#define DEFAULT_SIZE 10
+#define size_cmp(default, given)  \
+  ({                              \
+    size_t ret_val;               \
+    do {                          \
+      if (default < given) {      \
+        ret_val = given;          \
+      } else {                    \
+        ret_val = default;        \
+      }                           \
+    } while (0);                  \
+    ret_val;                      \
+  })
 
-int* init(size_t size, int input) {
-  int* factor = malloc(sizeof(int) * size);
-  if (factor == NULL) return 0;
+// Source from:
+// https://stackoverflow.com/questions/14297169/how-to-make-an-array-return-type-from-c-function
+
+int *init(const size_t size, int input)
+{
+  // NOTE: C doesn't advocate returning the address of a local variable
+  //  outside of the scope of its bound function. If you intend to do this,
+  //  the variable should be declared with a static type.
+  //
+  //  This concept is similar to the lifetime expansion in Rust,
+  //  a technique to indicate to the compiler that the annotated variables
+  //  or fields (struct) must be callable at least after the
+  //  return instruction is invoked.
+
+  // FIXME: Error "Initializer element is not a compile-time constant with `malloc`".
+  //  A "large" object is never a constant expression in C, even if
+  //  the object declared as `const`. Const-qualified objects (of any type)
+  //  are not constants in C language terminology. They cannot be used in
+  //  initializers of objects with static storage duration, regardless of
+  //  their type.
+  //  Same error: an attempt to initialize a static object with a
+  //  non-constant.
+  /* static */ int *factor = malloc(sizeof(int) * size);
+  if (factor == NULL)
+  	return 0;
   for (size_t start = 0; start < size; start++) {
-      factor[start] = (int)size + input;
+  	factor[start] = (int)size + input;
   }
   return factor;
+}
+
+typedef struct Custom_Array {
+  size_t size;
+  int arr[DEFAULT_SIZE];
+} Custom_Array;
+
+Custom_Array new_arr(size_t size_, int arr_[])
+{
+  Custom_Array a = {0};
+#ifdef DEFAULT_SIZE
+  size_ = size_cmp(DEFAULT_SIZE, size_);
+#endif
+  arr_ = &arr_[size_];
+
+  a.size = size_;
+  // NOTE: `sizeof` returns size of `*int` not `&int`.
+  if (sizeof(a.size) != sizeof(&arr_)) {
+  	return a;
+  }
+  for (size_t i = 0; i < size_; i++) {
+  	a.arr[i] = arr_[i];
+  }
+  return a;
+}
+
+void returns_an_arr(void (*accept)(void *))
+{
+  char arr[] = "Something else!!!";
+  accept(arr);
+}
+
+void on_accept(void *res)
+{
+  puts((char *)res);
 }
 
 // NOTE: To compile C code in Windows, please use:
 //  + Prompt: Visual Studio 2017 Developer Command Prompt v15.0.
 //  + Command: `cl /W4 main.c && main.exe`.
 
-int main() {
+int main(void)
+{
+  // 01292023:
   printf("Hello World\n");
   size_t size = 10;
   int input = 10;
-  int* f = init(size, input);
+  int *f = init(size, input);
   for (size_t i = 0; i < size; i++) {
-      // NOTE: Access element through its index doesn't require indirection
-      // operator. Syntax error: `*f[i]`.
-      printf("%d\n", f[i]);
+    // NOTE: Access element through its index doesn't require indirection
+    //  operator. Syntax error: `*f[i]`.
+    printf("%d\n", f[i]);
   }
   free(f);
+
+  // 01302023:
+  returns_an_arr(on_accept);
+
+  // FIXME: Error "Signal: segmentation fault (core dumped)".
+  size_t n_size = 50;
+  int empty_arr[n_size];
+  Custom_Array arr = new_arr(n_size, empty_arr);
+  for (size_t i = 0; i < n_size; i++) {
+    arr.arr[i] = DEFAULT_SIZE;
+  }
+  printf("%d\n", (int)arr.size);
+
   return 0;
 }
