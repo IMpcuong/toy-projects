1f9aaae9ee1d7ee28f963253b5a0e34a0164ef51
IMpcuong <trinhtiencuong_t63@hus.edu.vn>
2023-02-03 09:32:03 +0700

    doc: Directive `#pragma once` deobfuscate

--- clang-the-hardway/main.h
+++ clang-the-hardway/main.h
@@ -1,5 +1,8 @@
-#ifndef main_h_
-#define main_h_
+// NOTE: Having the same semantic with `#ifndef ... #define` block statement.
+// #pragma once
+
+#ifndef MAIN_H_
+#define MAIN_H_
 
 int double_input_as_ref(int x);
 void double_input_as_orig(int *x);
