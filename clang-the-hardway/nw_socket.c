b1354242a987267e91be94088ace91684046fd1d
IMpcuong <trinhtiencuong_t63@hus.edu.vn>
2023-02-03 14:59:26 +0700

    doc(address): IPv4+v6 representation form, converts between each version

--- clang-the-hardway/nw_socket.c
+++ clang-the-hardway/nw_socket.c
@@ -42,4 +42,50 @@
   * - Preference domain for each protocol:
   *   + TCP: Chat messages, etc.
   *   + UDP: Sending 40 positional update per second of the players (game domain), etc.
+  *
+  * - Data Encapsulation (flow chart):
+  *   Packet born -> packet wrapped ("encapsulated") in a header (rarely footer) in the first protocol (e.g. TFTP protocol)
+  *               -> then the whole thing (TFTP header included) is encapsulated again by the next protocol (e.g. UDP)
+  *               -> then again by the next (IP)
+  *               -> finallize by the hardware-based (physical-based) protocol (e.g. Ethernet).
+  *   Packet receive -> hardware strips the Ethernet header
+  *                  -> the kernel strips the IP and UDP headers
+  *                  -> the TFTP strips the TFTP header
+  *                  -> finally has the data for user.
+  *
+  * - Layer Network Model (aka OSI/ISO): this model describes a system of network functionally that has many advantages over other models.
+  *   For instance, you can write sockets programs that are exactly the same without caring how the data physically transmitted
+  *   (serial, thin Ethernet, AUI, etc) because program on the low-levels will deal with it for you. The actual network hardware and topology is transparent
+  *   to the socket programmers.
+  *   7 Layers of the full-blown model:
+  *     + Application (users interact with the network)
+  *     + Presentation
+  *     + Session
+  *     + Transport
+  *     + Network
+  *     + Data Link
+  *     + Physical (serial, Ethernet, etc)
+  *   A layered model more consistent with Unix might be:
+  *     + Application Layer (telnet, ftp, etc)
+  *     + Host-to-Host Transport Laye (TCP, UDP)
+  *     + Internet Layer (IP and routing)
+  *     + Network Access Layer (Ethernet, wi-fi, etc)
+  *   The kernel builds the Transport Layer and the Internet Layer on for us, and the hardware does the Network Access Layer.
+  *   The routers strips the packet to the IP header, consults its routing table (https://www.rfc-editor.org/rfc/rfc791).
+  *
+  * - IP addresses and ports sorted out, how the sockets API stores and manipulates IP addresses and other data:
+  *   + Network routing system "The Internet Protocol Version 4 ~ IPv4": where addresses made up for 4 bytes ~ 4 octets (8 bits * 4 octets := 32 bits),
+  *     commonly written in "dots and numbers" form: `192.0.12.111`. Total amount of addresses IPv4 system can be provided ~ 2^32 abilities.
+  *   + "The Internet Protocol Version 6 ~ IPv6": 2^128 addresses in the hexadecimal representation, with each 2-byte chunk separated by a colon. E.g.:
+  *     `2001:0db8:c9d2:aee5:73e3:934a:a5ae:9551`
+  *
+  *     `2001:0db8:c9d2:0012:0000:0000:0000:0051`
+  *     `2001:db8:c9d2:12::51`
+  *
+  *     `2001:0db8:ab00:0000:0000:0000:0000:0000`
+  *     `2001:db8:ab00::`
+  *
+  *     `0000:0000:0000:0000:0000:0000:0000:0001`
+  *     `::1` is the loopback address. It always means “this machine I’m running on now”. In IPv4, the loopback address is `127.0.0.1`.
+  *   + Conversion between IPv4/IPv6: `192.0.2.33` <-> `::ffff:192.0.2.33`.
   */
