e6484af1d018c7c7f6b9fdd7c272f06ee96636bd
IMpcuong <trinhtiencuong_t63@hus.edu.vn>
2023-02-05 22:55:31 +0700

    fix(warn): Clang compiler warnings resolution with some tricks

--- clang-the-hardway/array_as_return_type.c
+++ clang-the-hardway/array_as_return_type.c
@@ -2,8 +2,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#ifdef _WIN32
+#pragma warning(disable : 4710)
+#pragma warning(disable : 4204)
+#endif
+
 #define DEFAULT_SIZE 10
 
+// NOTE: Analogous with this macro rule as below.
+// #define SIZEOF_ARR(arr) ((size_t)(sizeof arr) / sizeof arr[0])
 #define SIZEOF_ARR(arr) ((size_t)(sizeof(arr) / sizeof(*arr)))
 #define CMP_PAIR(lhs, rhs) (lhs == rhs ? "True" : "False")
 
@@ -41,52 +48,63 @@
 
 // NOTE: Follows `winsock.h` coding conventions.
 #undef wSIZE_CMP
-#define wSIZE_CMP(orig, given, __ret_val) do { \
-  (orig < given) ? (__ret_val = given) : (__ret_val = orig); \
-} while (0)
+#define wSIZE_CMP(orig, given, __ret_val)                      \
+  do                                                           \
+  {                                                            \
+    (orig < given) ? (__ret_val = given) : (__ret_val = orig); \
+  } while (0)
 
-#define lSIZE_CMP(orig, given, __ret_val) do { \
-  if (orig < given) { \
-    __ret_val = given; \
-  } else { \
-    __ret_val = orig; \
-  } \
-} while(0)
+#define lSIZE_CMP(orig, given, __ret_val) \
+  do                                      \
+  {                                       \
+    if (orig < given)                     \
+    {                                     \
+      __ret_val = given;                  \
+    }                                     \
+    else                                  \
+    {                                     \
+      __ret_val = orig;                   \
+    }                                     \
+  } while (0)
 
 // Source from:
 // https://stackoverflow.com/questions/14297169/how-to-make-an-array-return-type-from-c-function
 
-int *init_arr(const size_t size, /* default = 0 */ int input)
+static int *init_arr(const size_t size, /* default = 0 */ const int input)
 {
   // NOTE: C doesn't advocate returning the address of a local variable
-  //  outside of the scope of its bound function. If you intend to do this,
-  //  the variable should be declared with a static type.
+  //    outside of the scope of its bound function. If you intend to do this,
+  //    the variable should be declared with a static type.
   //
-  //  This concept is similar to the lifetime expansion in Rust,
-  //  a technique to indicate to the compiler that the annotated variables
-  //  or fields (struct) must be callable at least after the
-  //  return instruction is invoked.
+  //    This concept is similar to the lifetime expansion in Rust,
+  //    a technique to indicate to the compiler that the annotated variables
+  //    or fields (struct) must be callable at least after the
+  //    return instruction is invoked.
 
   // FIXME: Error "Initializer element is not a compile-time constant with `malloc`".
-  //  A "large" object is never a constant expression in C, even if
-  //  the object declared as `const`. Const-qualified objects (of any type)
-  //  are not constants in C language terminology. They cannot be used in
-  //  initializers of objects with static storage duration, regardless of
-  //  their type.
-  //  Same error: an attempt to initialize a static object with a
-  //  non-constant.
-  if (0 == size) return 0;
-  /* static */ int *factor = malloc(sizeof(int) * size);
+  //    A "large" object is never a constant expression in C, even if
+  //    the object declared as `const`. Const-qualified objects (of any type)
+  //    are not constants in C language terminology. They cannot be used in
+  //    initializers of objects with static storage duration, regardless of
+  //    their type.
+  //    Same error: an attempt to initialize a static object with a
+  //    non-constant.
+  if (0 == size)
+    return 0;
+  int *factor = malloc(sizeof(int) * size);
   if (NULL == factor)
-  	return 0;
-  for (size_t i = 0; i < size; i++) {
-  	factor[i] = (int)size + input;
+    return 0;
+
+  for (size_t i = 0; i < size; i++)
+  {
+    factor[i] = (int)size + input;
   }
   return factor;
 }
 
 typedef struct Custom_Array Custom_Array, *ca;
-struct Custom_Array {
+struct Custom_Array
+{
   size_t size;
   int *child_arr;
 
@@ -106,20 +124,21 @@ void ca_print_childs(Custom_Array *self)
     printf("Zero-length array: %zu", self->size);
 
   printf("%s\n", CMP_PAIR(self->size, SIZEOF_ARR(self->child_arr)));
-  for (size_t i = 0; i < self->size; i++) {
+  for (size_t i = 0; i < self->size; i++)
+  {
     printf("Idx: %zu / Val: %d\n", i, (self->child_arr)[i]);
   }
 }
 
 // NOTE: `int *arr_` -> `SIZEOF_ARR(arr_) = 2`.
 //       `int **arr_` -> `SIZEOF_ARR(arr_) = 1`.
-struct Custom_Array *new_custom_arr(size_t size_, int *arr_)
+static inline Custom_Array *new_custom_arr(size_t size_, int *arr_)
 {
   size_t __arg_arr_size = SIZEOF_ARR(arr_);
   printf("Arg-array's len = %zu\n", __arg_arr_size);
 
   // Alter native method for `new` keyword in C:
-  // int* p = malloc(sizeof(*p)); // int* p = new int;
+  // int *p = malloc(sizeof(*p)); // int* p = new int;
   // ...
   // free(p);
 
@@ -134,28 +153,33 @@ struct Custom_Array *new_custom_arr(size_t size_, int *arr_)
 
   Custom_Array *__custom = {0};
   // FIXME: Cannot reassign the array argument's size.
-  //  Because it already has a fixed size.
-  // arr_ = &arr_[size_]; // Correct syntax.
+  //    Because it already has a fixed size.
+  // arr_ = &arr_[size_]; // Correct syntax, but wrong semantic.
 
   // FROM: https://stackoverflow.com/questions/11207783/read-and-write-to-a-memory-location
   __custom = (Custom_Array *)malloc(sizeof(Custom_Array) * 1);
-  __custom->size = out_;
+  if (__custom)
+  {
+    __custom->size = out_;
+    // NOTE: `sizeof` returns size of `*int` not `&int`, valid e.g. `sizeof(&arr_)`.
+    // NOTE: `sizeof(int)` with a type name, the name must be enclosed in parentheses.
+    //       `sizeof <expr>` when used with an expression, can be specified with or without parentheses.
+    //       `sizeof` is a operator.
+    __custom->child_arr = init_arr(__custom->size, (int)__arg_arr_size);
+    printf("__custom := (size / first) ~ (%zu / %d)\n", __custom->size, *__custom->child_arr);
 
-  // NOTE: `sizeof` returns size of `*int` not `&int`, valid e.g. `sizeof(&arr_)`.
-  // NOTE: `sizeof(int)` == `sizeof int`, both syntax are excepted.
-
-  __custom->child_arr = init_arr(__custom->size, (int)__arg_arr_size);
-  printf("__custom := (size / first) ~ (%zu / %d)\n", __custom->size, *__custom->child_arr);
-  return __custom;
+    return __custom;
+  }
+  return NULL;
 }
 
-void returns_an_arr(void (*accept)(void *))
+static inline void returns_an_arr(void (*accept)(void *))
 {
   char arr[] = "Something else!!!";
   accept(arr);
 }
 
-void on_accept(void *res)
+static inline void on_accept(void *res)
 {
   puts((char *)res);
 }
@@ -163,6 +187,7 @@ void on_accept(void *res)
 // NOTE: To compile C code in Windows, please use:
 //  + Prompt: Visual Studio 2017 Developer Command Prompt v15.0.
 //  + Command: `cl /W4 main.c && main.exe`.
+//             `cl /O2 /analyze:WX- /Ze /Wall array_as_return_type.c /o aart && aart.exe`.
 
 int main(void)
 {
@@ -172,9 +197,10 @@ int main(void)
   size_t size = 10;
   int input = 10;
   int *f_arr = init_arr(size, input);
-  for (size_t i = 0; i < size; i++) {
+  for (size_t i = 0; i < size; i++)
+  {
     // NOTE: Access element through its index doesn't require indirection
-    //  operator. Syntax error: `*f[i]`.
+    //    operator. Syntax error: `*f[i]`.
     printf("Part 1: %d\n", f_arr[i]);
   }
 
@@ -188,18 +214,22 @@ int main(void)
   // printf("%zd\n", y);  // Prints as signed decimal.
 
   // FIXME: Error "Signal: segmentation fault (core dumped)".
-  const size_t n_size = 50;
+  static size_t N_SIZE = 50;
+  int *ca_childs = init_arr(N_SIZE, 0);
   Custom_Array ca = {
-    .size = n_size,
-    .child_arr = init_arr(ca.size, 0),
+      .size = N_SIZE,
+      .child_arr = (int *)(0),
+      .print_childs = NULL,
   };
-  printf("Initialize `Custom_Array ca`: (size / first) ~ (%zu / %d)\n", ca.size, *ca.child_arr);
+  ca.child_arr = ca_childs;
 
   // NOTE: Struct methods implementation in C.
+  //    No needed anymore, when the struct initializer has already do this.
   ca.print_childs = ca_print_childs; // Function assignment.
   ca.print_childs(&ca);
+  printf("Initialize `Custom_Array ca`: (size / first) ~ (%zu / %d)\n", ca.size, *ca.child_arr);
 
-  Custom_Array *arr = new_custom_arr(n_size, ca.child_arr);
+  Custom_Array *arr = new_custom_arr(N_SIZE, ca.child_arr);
   printf("`Custom_Array *arr` destructuring: (size, arr_ptr) = (%zu, %p)\n", arr->size, arr->child_arr);
 
   arr->print_childs = ca_print_childs;
