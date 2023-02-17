fe34889fac8b852abef6b13a55539b4c2c3d2318
IMpcuong <trinhtiencuong_t63@hus.edu.vn>
2023-02-17 21:00:51 +0700

    fix(script): Return when error code appear

--- clang-the-hardway/run.sh
+++ clang-the-hardway/run.sh
@@ -3,17 +3,27 @@
 declare -r outfile="out"
 declare -x filename="$1"
 
-# NOTE: Identical with `test -n "$filename"`.
+# NOTE: `-e "$filename"` is identical with `test -n "$filename"`.
 if [[ -e "${filename:-}" && "$filename" == *".c"* ]]; then
   gcc -Wall -Wextra -pedantic \
-    -Wshadow -Wformat=2 -Wconversion \
-    -Wpointer-arith -Wredundant-decls -Wnested-externs \
-    -Wno-long-long -Wno-long-long \
-    -std=c99 -O2 \
-    -o "$outfile" "$filename"
+  -Wshadow -Wformat=2 -Wconversion \
+  -Wpointer-arith -Wredundant-decls -Wnested-externs \
+  -Wno-long-long -Wno-long-long \
+  -std=c99 -O2 \
+  -o "$outfile" "$filename"
 else
   printf "ERROR: Please provide a C source code file to serve the compiler\n"
+  exit 1
 fi
 
-[[ -e "$outfile" ]] || printf "ERROR: Cannot locate the binaries executable file %s" "$outfile"
+[[ -e "$outfile" ]] || {
+  printf "ERROR: Cannot locate the binaries executable file %s" "$outfile"
+  exit 1
+}
 ./"$outfile"
+
+[[ "$?" == 0 ]] || {
+  printf "ERROR: Runtime error with %s" "$outfile"
+  exit 1
+}
+rm -f "$outfile" && exit 0
