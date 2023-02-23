daf8eb8db6c3bb097819543e93e3960e78774e0e
IMpcuong <trinhtiencuong_t63@hus.edu.vn>
2023-02-23 17:59:37 +0700

    learn(padding): Memory alignment padding by default

--- clang-the-hardway/polymorphism_and_alignment.c
+++ clang-the-hardway/polymorphism_and_alignment.c
@@ -40,6 +40,31 @@ void set_value_from(MyHeader *h, double value)
     ((MyStructFloat *)h)->value = (float)value;
 }
 
+// 1: Alignment (memory) makes things more complicated:
+
+typedef struct
+{
+  unsigned char a;     // 1 byte.
+  unsigned int b;      // 4 bytes (32 bits) -> Want to live in a `even` 4 byte addresses (addr % 4 == 0).
+} AlignmentTestStruct; // Calculation: 5 bytes -> Reality: 8 bytes.
+
+// Avoid wasting free memory (ordering matter):
+typedef struct
+{
+  unsigned char a;
+  unsigned char x;
+  unsigned char y;
+  unsigned char z;
+  unsigned int b;
+} UsingFreedStruct;
+
+// Ordering won't always save your memory:
+typedef struct
+{
+  unsigned int b;  // 4 bytes.
+  unsigned char a; // 1 byte.
+} OrderingTestStruct;
+
 int main(void)
 {
   MyStructInt *mst_intptr = &(struct MyStructInt){
@@ -61,4 +86,19 @@ int main(void)
   set_value_from(&mst_intptr->header, ~(1 << 31));
   set_value_from(&mst_floatptr->header, ~(1 << 31));
   printf("Integer: %d | Float: %.3f (%zu)\n", mst_intptr->value, mst_floatptr->value, sizeof(float));
+
+  // NOTE: `***` == `pad` (padding operation).
+  // NOTE: Padding `AlignmentTestStruct` to := `a***bbbb`.
+  //    That means `b` wants to be stored on the location (index) divisible by 4.
+  printf("True: %d\n", sizeof(AlignmentTestStruct) == 8);
+
+  // NOTE: `UsingFreedStruct` := `axyzbbbb`.
+  printf("True: %d\n", sizeof(UsingFreedStruct) == 8); // The same size.
+
+  // NOTE: Padding `OrderingTestStruct` to := `bbbba***`.
+  //    The reason: it's always possible to have an adjacent struct right after the previous one
+  //    := `bbbba***bbbba***` (yeah, array is suck).
+  printf("True: %d\n", sizeof(OrderingTestStruct) == 8);
+
+  // NOTE: `sizeof` always give you a value that you can repeat
 }
