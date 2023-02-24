315ed4bbf57f12ff801650e774429f00cdafeacb
IMpcuong <trinhtiencuong_t63@hus.edu.vn>
2023-02-24 10:27:23 +0700

    test(order): Ordering matter with all struct fields

--- clang-the-hardway/test.c
+++ clang-the-hardway/test.c
@@ -1,10 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct
+{
+  char first;
+  int middle;
+  char last;
+
+} OrderingVolatility;
+
+typedef struct
+{
+  char first;
+  char middle;
+  int last;
+
+} OrderingMatter;
+
 int main(void)
 {
   size_t num = 4;
-  fprintf(stdout, "%zu", sizeof num);
+  fprintf(stdout, "%zu\n", sizeof num);
+
+  size_t ordered = sizeof(OrderingMatter);
+  size_t volatility = sizeof(OrderingVolatility);
+  char *is_greater = (ordered < volatility) ? "True" : "False";
+  printf("`sizeof(OrderingMatter) < sizeof(OrderingVolatility)`: %s (%zu < %zu)\n",
+         is_greater, ordered, volatility);
 
   return 0;
 }
