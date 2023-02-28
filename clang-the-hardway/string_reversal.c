5708db44052085754a0f9f2f6ed16b813dc99846
IMpcuong <trinhtiencuong_t63@hus.edu.vn>
2023-02-28 16:03:08 +0700

    learn(bits): Reverse bits order using bitwise-operator

--- clang-the-hardway/string_reversal.c
+++ clang-the-hardway/string_reversal.c
@@ -1,6 +1,38 @@
 #include <stdio.h>
+#include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
+
+// NOTE: Duplicated with the implementation in `main.c`.
+char *convert_to_binary(size_t _src_num)
+{
+  size_t size_t_bitlen;
+#if defined(__x86_64__)
+  size_t_bitlen = (sizeof _src_num) / 2 * 8; // 8 (size_t) / 2 * 8 (char) = 32.
+#elif defined(__i386__)
+  size_t_bitlen = (sizeof _src_num) * 8; // 4 (size_t) * 8 (char) = 32.
+#endif
+  size_t binary_str_bufsize = size_t_bitlen + 1;
+
+  char *binary_str;
+  // NOTE: Allocate a buffer to hold the binary string.
+  //    Expression `+ 1` := '\0' terminated character.
+  binary_str = malloc(/* optional := char ~ 1 byte */ sizeof(*binary_str) * binary_str_bufsize + 1);
+  if (binary_str == NULL)
+    fprintf(stderr, "Errno = %d\n", ENOMEM);
+  binary_str[0] = '\0';
+
+  // NOTE: Extract each bit of the number and add it to the binary string.
+  int i;
+  for (/* 31 -> 0 */ i = (int)size_t_bitlen - 1; i >= 0; i--)
+  {
+    if (_src_num & (1 << i))
+      strcat_s(binary_str, binary_str_bufsize, "1");
+    else
+      strcat_s(binary_str, binary_str_bufsize, "0");
+  }
+  return binary_str;
+}
 
 // Idea from: https://github.com/torvalds/linux/blob/master/arch/arm/boot/compressed/string.c
 size_t strlen_custom(char *src)
@@ -49,7 +81,37 @@ void reverse_str_method2(char *src, int length)
   reverse_str_method2(src + 1, length - 2);
 }
 
-unsigned int f_randi(unsigned int index)
+// FIXME: Reverse with an incorrect order.
+uint32_t reverse_bits_orderV1(uint32_t orig)
+{
+  uint32_t reverse = 0;
+  // Traversing bits from the right.
+  size_t i;
+  for (i = 0; i < (sizeof orig) * 8; i++)
+  {
+    reverse <<= 1;
+    if (orig & 1)
+      reverse |= 1;
+    orig >>= 1;
+  }
+  return reverse;
+}
+
+uint32_t reverse_bits_orderV2(uint32_t orig)
+{
+  uint32_t reverse = 0;
+  // Traversing bits from the right.
+  while (orig > 0)
+  {
+    reverse <<= 1;
+    if (orig & 1)
+      reverse ^= 1;
+    orig >>= 1;
+  }
+  return reverse;
+}
+
+uint32_t f_randi(uint32_t index)
 {
   index = (index << 13) ^ index;
   return ((index * (index * index * 15731 + 789221) + 1376312589) & 0x7fffffff);
@@ -62,7 +124,7 @@ int main(void)
 
   char test_string0[128] = "IMpcuong";
   reverse_str_method0(test_string0);
-  printf("Reversal method 1: %s\n", test_string0);
+  printf("Reversal method 0: %s\n", test_string0);
 
   char test_string1[128] = "IMpcuong";
   reverse_str_method1(test_string1);
@@ -70,10 +132,21 @@ int main(void)
 
   char test_string2[128] = "IMpcuong";
   reverse_str_method2(test_string2, (int)strlen_custom(test_string2));
-  printf("Reversal method 1: %s\n", test_string2);
+  printf("Reversal method 2: %s\n", test_string2);
 
-  unsigned int rand = f_randi(0xFFFF);
+  uint32_t rand = f_randi(0xFFFF);
   printf("Randomize algorithm output: %d\n", rand);
+
+  unsigned int zero = 0;
+  printf("%d: %s\n", zero << 1, convert_to_binary((size_t)(zero << 1)));
+
+  uint32_t orig = 0b11110000; // (0x80 == 128) := 8 bits.
+  printf("%d: %s\n", orig, convert_to_binary((size_t)orig));
+  uint32_t reverse1 = reverse_bits_orderV1(orig);
+  uint32_t reverse2 = reverse_bits_orderV2(orig);
+  printf("%d / %d: %s / %s\n", reverse1, reverse2,
+         convert_to_binary((size_t)reverse1),
+         convert_to_binary((size_t)reverse2));
 
   return 0;
 }
