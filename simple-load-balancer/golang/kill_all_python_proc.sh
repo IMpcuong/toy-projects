983fe147d11519da53f3d5832221f80f9195cf04
IMpcuong <trinhtiencuong_t63@hus.edu.vn>
2023-03-27 09:58:39 +0700

    scripts: Serving server-list's args using for-loop

--- simple-load-balancer/golang/kill_all_python_proc.sh
+++ simple-load-balancer/golang/kill_all_python_proc.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ps aux | grep -i python | awk '{ print $2 }' | xargs kill -9
+ps aux | grep -i python | awk '{ print $2 }' | tail -n+2 | xargs kill -9
--- simple-load-balancer/golang/run.sh
+++ simple-load-balancer/golang/run.sh
@@ -3,13 +3,18 @@
 go build .
 
 declare -a ports=(3031 3032 3033 3034 3035)
+declare -x servers="http://localhost:${ports[0]}"
 for i in ${!ports[@]}; do
   curl -vv "http://localhost:${ports[i]}"
   if [[ "$?" != 0 ]]; then
     # NOTE: Python version < 3.
     # python -m SimpleHTTPServer ${ports[i]} &
     python3 -m http.server ${ports[i]} &
+    if (($i > 0)); then
+      servers+=",http://localhost:${ports[i]}"
+    fi
   fi
 done
 
-"./slb" --backends="http://localhost:3031,http://localhost:3032,http://localhost:3033,http://localhost:3034,http://localhost:3035"
+declare -x binfile=$(grep -E "module\s.+$" go.mod | cut -d' ' -f2)
+"./slb" --servers="${servers}"
