5a26ee0a8c61f474771baab2c5d50eb3c1c5c36e
IMpcuong <trinhtiencuong_t63@hus.edu.vn>
2023-03-29 16:14:04 +0700

    task: Lambda expressions examination in Python; diassembler library

--- everyday-coding/03242023/main.py
+++ everyday-coding/03242023/main.py
@@ -1,3 +1,4 @@
+import dis
 import logging
 import os
 import pprint
@@ -14,8 +15,18 @@ def main() -> None:
 
   os_attrs_dict = convert_os_attributes()
 
+  # NOTE(impcuong): Testing lambda expressions.
+  x = (lambda num: num ** 2)(3)
+  is_even = lambda num: num % 2 and 'even' or 'odd'
+  even_or_not = is_even(1 << 2)
+
   pprint.pprint(list_int, indent=2, stream=sys.stdout)
   pprint.pprint(os_attrs_dict, indent=2, stream=sys.stdout)
+  pprint.pprint(x, indent=2, stream=sys.stdout)
+
+  # NOTE(impcuong): Disassemble library `dis` using to convert ASM expressions into human-readable instructions.
+  pprint.pprint(dis.dis(is_even), indent=2, stream=sys.stdout)
+  pprint.pprint(even_or_not, indent=2, stream=sys.stdout)
 
 
 def division_comprehension(list_int: typing.List[int]) -> typing.List[int]:
