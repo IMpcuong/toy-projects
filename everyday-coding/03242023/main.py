d4515a9111a797f250567e72dafbc1c6db95dfe1
IMpcuong <trinhtiencuong_t63@hus.edu.vn>
2023-04-21 18:18:12 +0700

    fix: future_to_url 'NoneType' object is not iterable

--- everyday-coding/03242023/main.py
+++ everyday-coding/03242023/main.py
@@ -1,13 +1,24 @@
+import concurrent.futures
 import dis
 import logging
 import os
 import pathlib
 import pprint
+import requests as rq
 import sys
 import typing
+import urllib.request
 
 
 logger = logging.getLogger(__name__)
+
+
+URLS: typing.List[str] = [
+    "https://www.foxnews.com/",
+    "https://www.cnn.com/",
+    "https://europe.wsj.com/",
+    "https://www.bbc.co.uk/",
+]
 
 
 def main() -> None:
@@ -35,6 +46,11 @@ def main() -> None:
     Executioner.print_paths_class()
     butcher.print_paths_static("Welcome to the hotel hell!")
 
+    cmd = ["curl", "-sSL"]
+    Executioner.request_domain(cmd, URLS)
+    # https://docs.python.org/3/library/concurrent.futures.html?highlight=concurrent%20futures%20threadpoolexecutor#concurrent.futures.ThreadPoolExecutor
+    Executioner.request_stress_test(5, 5, Executioner.load_url)
+
 
 def division_comprehension(list_int: typing.List[int]) -> typing.List[int]:
     list_int = [v for i, v in enumerate(list_int) if v % 2 or v % 4 == 0]
@@ -56,8 +72,8 @@ def convert_os_attributes() -> typing.Dict[str, str]:
 
 class Executioner(object):
     # Global fields/attributes:
-    pid = 0
-    paths = []
+    pid: int = 0
+    paths: typing.Dict[str, str] = {}
 
     def __init__(
         self,
@@ -75,7 +91,7 @@ class Executioner(object):
 
         parent_dir_path = "../.."
         glob = "*"
-        list_dirs_lookup = []
+        list_dirs_lookup: typing.List[pathlib.Path] = []
         if lookup_path_pattern.__contains__(glob):
             list_dirs_lookup = sorted(
                 pathlib.Path(parent_dir_path).rglob(lookup_path_pattern)
@@ -95,6 +111,54 @@ class Executioner(object):
 
     def __eq__(self, __other: object) -> bool:
         return len(self.paths) == len(__other.paths)
+
+    def request_response(rq: rq.Response) -> None:
+        if (rq.status_code >= 400) and (rq.status_code <= 550):
+            print("Failed with status code:", rq.status_code)
+        print("Successful with status code:", rq.status_code)
+
+    @classmethod
+    def request_domain(
+        cls, commands: typing.List[str], urls: typing.List[str]
+    ) -> typing.List[rq.Request]:
+        list_rq: typing.List[rq.Request] = []
+        if urls[0] and not urls[0].isspace():
+            for url in urls:
+                r = rq.request("GET", url, headers={}, data="")
+                cls.request_response(r)
+                list_rq.append(r)
+        else:
+            pprint.pprint(f">> {isinstance(cls.paths, typing.List)}")
+            cls.paths["gg"] = "https://google.com"
+            cls.paths["cmd"] = "".join(map(lambda opt: opt.upper(), commands[1:]))
+            pprint.pprint(f'>> {cls.paths.get("cmd")}')
+            r = rq.request("GET", cls.paths.get("gg"), headers={}, data="")
+            cls.request_response(r)
+            list_rq.append(r)
+        return list_rq
+
+    @classmethod
+    def load_url(cls, url: str, timeout: int):
+        with urllib.request.urlopen(url, timeout=timeout) as conn:
+            return conn.read()
+
+    @classmethod
+    def request_stress_test(
+        cls, max_threads: int, _concur: int, func: typing.Callable
+    ) -> None:
+        with concurrent.futures.ThreadPoolExecutor(max_threads) as executor:
+            future_to_url = {
+                executor.submit(func, url, 60): url
+                for url in URLS.extend([cls.paths.get("gg")])
+            }
+            for f in concurrent.futures.as_completed(future_to_url):
+                url = future_to_url[f]
+            try:
+                data = f.result()
+            except Exception as expt:
+                print("%r generated an exception: %s" % (url, expt))
+            else:
+                print("%r page is %d bytes" % (url, len(data)))
 
     # NOTE: https://stackoverflow.com/a/1669524/12535617
     def print_paths(self) -> None:
