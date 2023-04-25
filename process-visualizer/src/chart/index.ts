51d4e3230663bedf2a131e5f3bebe7bf98f75870
IMpcuong <trinhtiencuong_t63@hus.edu.vn>
2023-04-25 13:17:32 +0700

    optim: Coerced with String wrapper instead of raw string; variable type demonstration

--- process-visualizer/src/chart/index.ts
+++ process-visualizer/src/chart/index.ts
@@ -1,17 +1,17 @@
 const PROC_API = 'http://localhost:9999';
 
 async function collectProcessStats(scheme: string, host: string, port: number): Promise<string> {
-  let endpoint = "";
+  let endpoint = new String();
   if (!scheme && !host && !port) {
     endpoint = scheme.concat("://" + host + ':' + port.toString());
   } else {
     endpoint = PROC_API;
   }
 
-  const response = await fetch(endpoint);
+  const response = await fetch(endpoint.toString());
   const result: string = response.ok ? await response.text() : response.statusText;
   return result;
 }
 
-const procStats: Promise<string> = collectProcessStats("http", "localhost", 9999);
-console.log(`Process stats: ${await procStats}`);
+const procStats: string = await collectProcessStats("http", "localhost", 9999);
+console.log(`Process stats: ${procStats}`);
