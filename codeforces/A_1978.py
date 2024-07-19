7a241e229b82013a60f1db9d1b9a03d1a68bc06e
IMpcuong <trinhtiencuong_t63@hus.edu.vn>
2024-07-19 16:12:09 +0700

    algo(codeforces): My journey is just beginning (day5)

---
+++ codeforces/A_1978.py
@@ -0,0 +1,7 @@
+# Link: https://codeforces.com/problemset/problem/1978/A
+
+t = int(input())
+for _ in range(t):
+    books = int(input())
+    pages = list(map(int, input().split()))
+    print(pages[books - 1] + max(pages[:books - 1]))
