52b06869c06826cb3948048cfc72d5890ef4aaca
IMpcuong <trinhtiencuong_t63@hus.edu.vn>
2024-08-29 18:17:35 +0700

    algo(codeforces): My journey is just beginning (day14)

---
+++ codeforces/A_1881.py
@@ -0,0 +1,17 @@
+# Link: https://codeforces.com/problemset/problem/1881/A
+
+t = int(input())
+while t > 0:
+    l_x, l_s = map(int, input().split())
+    x, s = input(), input()
+    ops = 0
+    while s not in x:
+        if l_x > l_s:
+            x += x
+            ops += 1
+            break
+        x += x
+        l_x <<= 1 # l_x = l_x * 2
+        ops += 1
+    print(ops if s in x else -1)
+    t -= 1
