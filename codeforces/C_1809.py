7724b5477f5b539670780547a19fad1f29e9fc6a
IMpcuong <trinhtiencuong_t63@hus.edu.vn>
2024-10-09 10:56:21 +0700

    algo(codeforces): My journey is just beginning (day32) (x2)

--- codeforces/C_1809.py
+++ codeforces/C_1809.py
@@ -12,6 +12,19 @@ input_to_list = lambda: list(input_to_map())
 def solve() -> list:
     n, k = input_to_map()
     res = [-2] * n
+    # NOTE:
+    # + [1, -1, -1] -> k = 1 satisfied the first condition.
+    #   However, the condition that all subsequences have a summary that is strictly different from zero
+    #   prevents us from using [-1] * n as the base answer.
+    # + [1, -2, -2] -> k = 1 satisfied.
+    # + [3, -2, -2] -> k = 2 satisfied.
+    # + [5, -2, -2] -> k = 3 satisfied.
+    #   -> We use -2 as the basis element to apply the odd and even rule.
+    #   -> res[i] = 2 * k - 1.
+    # + [5, 1, -2] -> k = 4 satisfied && k > n.
+    #   -> The first element now point the next index by shifting the array by 1 unit.
+    #   -> n -= 1.
+    #   -> res[i] = 2 * k - 1.
     i = 0
     while k > 0:
         res[i] = 2 * k - 1
