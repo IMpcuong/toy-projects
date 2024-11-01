a8d7082cb0d163fa85c5b88450d917d55f8e22c6
IMpcuong <trinhtiencuong_t63@hus.edu.vn>
2024-11-01 17:55:41 +0700

    algo(codeforces): My journey is just beginning (day39) (crazy x2)

--- codeforces/C_1987.py
+++ codeforces/C_1987.py
@@ -12,12 +12,17 @@ input_to_list = lambda: list(input_to_map())
 def solve() -> int:
     n = input_to_int()
     heights = input_to_list()
+    # NOTE: dp[0] = k := a theoretical number
+    #   that represents the total number of steps required for all flowers[i + 1] to reach the zero level.
     dp = [0]
     for i in range(n - 1, -1, -1):
         if i == n - 1:
             dp[0] = heights[i]
         else:
             if heights[i] > heights[i + 1]:
+                # NOTE:
+                # + flowers[i + 1] := needs heights[i + 1] steps -> 0.
+                # + flowers[i + 1] := be blocked (dp[0] - heights[i + 1]) times.
                 block_times = dp[0] - heights[i + 1]
                 diff = heights[i] - heights[i + 1]
                 if block_times >= diff:
