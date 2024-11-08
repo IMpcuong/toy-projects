413f34bcf2f339b7f12c9cfa96f857ef1cfc439f
IMpcuong <trinhtiencuong_t63@hus.edu.vn>
2024-11-08 18:31:09 +0700

    algo(codeforces): My journey is just beginning (day40) (TLE)

--- codeforces/D_2033.py
+++ codeforces/D_2033.py
@@ -1,6 +1,7 @@
 # Link: https://codeforces.com/contest/2033/problem/D
 
 import sys
+from typing import Dict, List
 
 
 input = lambda: sys.stdin.readline().rstrip()
@@ -12,26 +13,57 @@ input_to_list = lambda: list(input_to_map())
 def solve() -> int:
     n = input_to_int()
     a = input_to_list()
+
+    map_remain_sum: Dict[int, List[int]] = {sum(a): [-1]}
+    s = sum(a[1:])
+    for i in range(n):
+        if i != 0:
+            s -= a[i]
+        if s not in map_remain_sum:
+            map_remain_sum[s] = []
+        map_remain_sum[s].append(i)
+    for indices in map_remain_sum.values():
+        indices.sort()
+    # print(map_remain_sum)
+
+    s_dp = sum(a[1:])
     dp = [0] * n
     for i in range(n):
         if i == 0:
             if a[i] == 0:
                 dp[0] = 1
         else:
-            s = sum(a[0 : i + 1])
-            segments = 0
-            if s == 0:
-                segments += 1
-            for j in range(i):
-                s -= a[j]  # s = sum(a[j + 1 : i + 1])
-                # print(f">>>>> i ~ {i} | j ~ {j} | s ~ {s}")
-                if s == 0:
-                    if segments < dp[j] + 1:
-                        segments = dp[j] + 1
-            if dp[i - 1] < segments:
-                dp[i] = segments
+            s_dp -= a[i]
+            # print(i, s_dp)
+            same_sum_indices = map_remain_sum[s_dp]
+            # print(same_sum_indices)
+            l = len(same_sum_indices)
+            tmp_idx = -n
+            for j in range(l):
+                start_zero_sum_seq = same_sum_indices[j]
+                end_zero_sum_seq = i
+                mark_start_zero_seq = same_sum_indices[j - 1]
+                if end_zero_sum_seq <= start_zero_sum_seq:
+                    if j == 0:
+                        break
+                    tmp_idx = mark_start_zero_seq
+                    same_sum_indices = same_sum_indices[j:]
+                    map_remain_sum[s_dp] = same_sum_indices
+                    break
+            # print(tmp_idx)
+            dp[i] = dp[i - 1]
+            if tmp_idx == -n:
+                continue
+            dp_tmp = 1
+            if tmp_idx == -1:
+                if dp_tmp > dp[i]:
+                    dp[i] = dp_tmp
+                # print("1>>>>>", dp[i])
             else:
-                dp[i] = dp[i - 1]
+                dp_tmp = dp[tmp_idx] + 1
+                if dp_tmp > dp[i]:
+                    dp[i] = dp_tmp
+                # print("2>>>>>", dp[i])
     return dp[-1]
 
 
