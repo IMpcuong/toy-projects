2fdd8cede337120f5a5aa699a84fe593c48c96d9
IMpcuong <trinhtiencuong_t63@hus.edu.vn>
2024-12-02 18:54:25 +0700

    algo(codeforces): My journey is just beginning (day44)

--- codeforces/D_2019.py
+++ codeforces/D_2019.py
@@ -9,28 +9,26 @@ input_to_map = lambda: map(int, input().split())
 input_to_list = lambda: list(input_to_map())
 
 
-def search_interval_range(nums: list[int], target: int) -> list[int]:
-    if nums.count(target) == 1:
-        pos = nums.index(target)
-        return [pos, pos]
-    first, last = -1, -1
-    for i in range(len(nums)):
-        if nums[i] == target:
-            if first == -1:
-                first = i
-            last = i
-    return [first, last]
+def search_all_interval_ranges(deadlines: list[int]) -> dict:
+    ans = dict()
+    for i, moment in enumerate(deadlines):
+        if moment not in ans:
+            ans[moment] = []
+        ans[moment].append(i)
+    for moment in ans:
+        first = ans[moment][0]
+        last = ans[moment][-1]
+        ans[moment] = [first, last]
+    return ans
 
 
-def solve() -> list:
+def solve() -> list[int]:
     n = input_to_int()
     conquer_times = input_to_list()
     desc_deadlines = sorted(list(set(conquer_times)), reverse=True)
-    map_deadline = dict()
-    for moment in conquer_times:
-        if moment not in map_deadline:
-            map_deadline[moment] = []
-        leftmost_idx, rightmost_idx = search_interval_range(conquer_times, moment)
+    map_deadline = search_all_interval_ranges(conquer_times)
+    for moment in map_deadline:
+        leftmost_idx, rightmost_idx = map_deadline[moment]
         cur_distance = rightmost_idx - leftmost_idx + 1
         if cur_distance > moment:
             return 0
@@ -46,51 +44,35 @@ def solve() -> list:
             else:
                 rightmost_idx = n - 1
         map_deadline[moment] = [leftmost_idx, rightmost_idx]
-    lo, ro, ko = 0, 0, 0
+    lo, ro = 0, 0
     for i, moment in enumerate(desc_deadlines):
         if i == 0:
             lo = map_deadline[moment][0]
             ro = map_deadline[moment][1]
-            ko = moment - 1
         else:
             ln = map_deadline[moment][0]
             rn = map_deadline[moment][1]
             kn = moment - 1
-            # print(">>>>>", kn, ln, rn)
             if ln >= lo and rn >= ro:
                 if ln + kn > ro:
                     return 0
                 else:
+                    ln = ln
                     rn = ro
-                    if lo + ko >= ln + kn:
-                        ln = lo
-                    else:
-                        ln = ln + kn - ko
             elif ln <= lo and rn <= ro:
-                if rn - kn < 0:
+                if rn - kn < lo:
                     return 0
                 else:
                     ln = lo
-                    if rn -kn <= ro - ko:
-                        rn = ro
-                    else:
-                        rn = rn - kn + ko
+                    rn = rn
             elif lo >= ln and ro <= rn:
                 ln = lo
                 rn = ro
             elif lo <= ln and ro >= rn:
-                if lo + ko >= ln + kn:
-                    ln = lo
-                else:
-                    ln = ln + kn - ko
-                if ro - ko <= rn + kn:
-                    rn = ro
-                else:
-                    rn = rn - kn + ko
+                ln = ln
+                rn = rn
             lo = ln
             ro = rn
-            ko = kn
-        print(i, ko, lo, ro)
     return ro - lo + 1
 
 
