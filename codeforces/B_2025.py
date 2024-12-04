dfdc2db847731a286b3a6af47484eccbada2ae96
IMpcuong <trinhtiencuong_t63@hus.edu.vn>
2024-12-04 19:19:10 +0700

    algo(codeforces): My journey is just beginning (day45)

--- codeforces/B_2025.py
+++ codeforces/B_2025.py
@@ -1,4 +1,4 @@
-# Link: https://codeforces.com/contest/1987/problem/C
+# Link: https://codeforces.com/problemset/problem/2025/B
 
 import sys
 
@@ -13,9 +13,17 @@ def solve() -> list[int]:
     pairs = input_to_int()
     _ = input_to_list()
     arr_k = input_to_list()
+    _mod_val = 10**9 + 7
+    max_k = max(arr_k)
+    tmp_arr = [1] * (max_k + 1)
+    for k in range(1, max_k + 1):
+        if k == 1:
+            tmp_arr[k] = 2 # n = 2 && k = 1
+        else:
+            tmp_arr[k] = (2 * tmp_arr[k - 1]) % _mod_val # 2^k = 2 * 2^(k - 1)
     ans = [0] * pairs
     for i, k in enumerate(arr_k):
-        ans[i] = (2**k) % (10**9 + 7)
+        ans[i] = tmp_arr[k]
     return ans
 
 
