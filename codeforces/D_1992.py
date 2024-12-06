65cefcf36d6d9591e525b1beda7bc1076116ba08
IMpcuong <trinhtiencuong_t63@hus.edu.vn>
2024-12-06 18:17:40 +0700

    algo(codeforces): My journey is just beginning (day46) (WIP)

---
+++ codeforces/D_1992.py
@@ -0,0 +1,9 @@
+# Link: https://codeforces.com/problemset/problem/1992/D
+
+import sys
+
+
+input = lambda: sys.stdin.readline().rstrip()
+input_to_int = lambda: int(input())
+input_to_map = lambda: map(int, input().split())
+input_to_list = lambda: list(input_to_map())
