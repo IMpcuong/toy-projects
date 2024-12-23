54f3eb97c99104e0084efebc1bc89330a91fcafd
IMpcuong <trinhtiencuong_t63@hus.edu.vn>
2024-12-23 10:00:18 +0700

    algo(codeforces): My journey is just beginning (day53) (x2)

--- codeforces/B_1686.py
+++ codeforces/B_1686.py
@@ -13,16 +13,17 @@ input_to_list = lambda: list(input_to_map())
 def solve() -> int:
     n = input_to_int()
     permutation = input_to_list()
-    list_indices_asc_by_value = sorted(range(n), key=lambda i: -permutation[i])
+    list_indices_desc_by_value = sorted(range(n), key=lambda i: -permutation[i])
     _invalid_val = -1
     counter = 0
-    for idx in list_indices_asc_by_value:
+    for idx in list_indices_desc_by_value:
         if idx == n - 1:
             continue
         cur_val = permutation[idx]
-        next_val = permutation[idx + 1]
-        if cur_val > next_val and next_val != _invalid_val:
+        adjacent_val = permutation[idx + 1]
+        if cur_val > adjacent_val and adjacent_val != _invalid_val:
             counter += 1
+            # NOTE: Marks those indices as removed/unusable for any next iterations.
             permutation[idx] = _invalid_val
             permutation[idx + 1] = _invalid_val
     return counter
