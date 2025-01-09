140bfa482faf4bbfa976e10672286318ec1e39fb
IMpcuong <trinhtiencuong_t63@hus.edu.vn>
2025-01-09 14:31:55 +0700

    learn(cpp): Unordered map/set iterator-ptr and const-auto-lvalue

---
+++ cpp-the-hardway/04.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+
+#define FLUSH_STDOUT_OF(m, k, v) \
+  std::cout                      \
+      << m.size() << " | "       \
+      << k << " : " << v         \
+      << "\n";
+
+int main()
+{
+  std::unordered_map<int, std::string> status_codes{
+      {200, "Success"},
+      {404, "This is not the page you're looking for"},
+  };
+  auto iter = status_codes.find(200);
+  if (iter != status_codes.end())
+    std::cout << iter->first << ": " << iter->second << "\n";
+
+  std::string void_msg = status_codes[500];
+  status_codes[502] = "Bad gateway";
+  for (auto const &[k, v] : status_codes)
+    FLUSH_STDOUT_OF(status_codes, k, v);
+
+  for (auto const &item : status_codes)
+    FLUSH_STDOUT_OF(status_codes, item.first, item.second);
+
+  std::unordered_map<int, std::string>::iterator m_iter;
+  for (m_iter = status_codes.begin(); m_iter != status_codes.end(); m_iter++)
+    FLUSH_STDOUT_OF(status_codes, m_iter->first, m_iter->second);
+
+  std::unordered_set<std::string> planets{
+      "Venus",
+      "Jupiter",
+      "Mars",
+  };
+  for (auto const &item : planets)
+    FLUSH_STDOUT_OF(planets, item.data(), item.capacity());
+  return 0;
+}
