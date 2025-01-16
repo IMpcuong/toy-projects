fb9340ef7adb468b5c1ec759fbfe9c6d1df7e8bb
IMpcuong <trinhtiencuong_t63@hus.edu.vn>
2025-01-16 11:00:23 +0700

    algo(codeforces): My journey is just beginning (day57)

---
+++ codeforces/A_1182.py
@@ -0,0 +1,19 @@
+# Link: https://codeforces.com/problemset/problem/1182/A
+
+import sys
+from collections import deque
+
+
+input = lambda: sys.stdin.readline().rstrip()
+input_to_int = lambda: int(input())
+input_to_map = lambda: map(int, input().split())
+input_to_list = lambda: list(input_to_map())
+
+
+def solve() -> int:
+    n = input_to_int()
+    if n % 2 != 0:
+        return 0
+    return 2 ** (n // 2)
+
+print(solve())
--- codeforces/test.py
+++ codeforces/test.py
@@ -15,39 +15,48 @@ def cal_maximum_valid_seq_starts_from(
     counter = 0
     for chr in next_sample:
         counter += 1
-        if chr == "n":
-            if len(dq) == 0:
+        if len(dq) == 0:
+            if chr == "n":
                 dq.appendleft(chr)
-            else:
+            elif chr == "a" or chr == "r" or chr == "e" or chr == "k":
                 prev_bot_score += 1
-        elif 0 < len(dq) < _desired_str_len:
+        elif len(dq) < _desired_str_len:
             top = dq[0]
-            if top == "n" and chr == "a":
-                if len(dq) == 2:
-                    prev_bot_score += 1
-                    continue
-                dq.appendleft(chr)
-            elif top == "a" and chr == "r":
-                if len(dq) == 3:
-                    prev_bot_score += 1
-                    continue
-                dq.appendleft(chr)
-            elif top == "r" and chr == "e":
-                if len(dq) == 4:
-                    prev_bot_score += 1
-                    continue
-                dq.appendleft(chr)
-            elif top == "e" and chr == "k":
-                if len(dq) == 5:
-                    prev_bot_score += 1
-                    continue
-                dq.appendleft(chr)
+            for target_chr_pos in range(_desired_str_len - 1):
+                target_chr = _desired_str[target_chr_pos]
+                next_target_chr = _desired_str[target_chr_pos + 1]
+                if top == target_chr:
+                    if chr == next_target_chr:
+                        dq.appendleft(chr)
+                    elif chr != next_target_chr and chr in _desired_str:
+                        prev_bot_score += 1
+            # if top == "n":
+            #     if chr == "a":
+            #         dq.appendleft(chr)
+            #     elif chr == "n" or chr == "r" or chr == "e" or chr == "k":
+            #         prev_bot_score += 1
+            # elif top == "a":
+            #     if chr == "r":
+            #         dq.appendleft(chr)
+            #     elif chr == "n" or chr == "a" or chr == "e" or chr == "k":
+            #         prev_bot_score += 1
+            # elif top == "r":
+            #     if chr == "e":
+            #         dq.appendleft(chr)
+            #     elif chr == "n" or chr == "a" or chr == "r" or chr == "k":
+            #         prev_bot_score += 1
+            # elif top == "e":
+            #     if chr == "k":
+            #         dq.appendleft(chr)
+            #     elif chr == "n" or chr == "a" or chr == "r" or chr == "e":
+            #         prev_bot_score += 1
         if len(dq) == _desired_str_len:
-            prev_human_score += 5
+            prev_human_score += _desired_str_len
             dq.clear()
-        if counter == len(next_sample) and 0 < len(dq) < _desired_str_len:
-            dq.reverse()
-            prev_tail = "".join(dq)
+    if counter == len(next_sample) and 0 < len(dq) < _desired_str_len:
+        prev_bot_score += len(dq)
+        dq.reverse()
+        prev_tail = "".join(dq)
     return (prev_human_score, prev_bot_score, prev_tail)
 
 
