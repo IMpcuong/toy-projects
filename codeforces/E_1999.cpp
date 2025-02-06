37adc5593af8afcb959d3c26c7342c2247b71a36
IMpcuong <trinhtiencuong_t63@hus.edu.vn>
2025-02-06 16:54:54 +0700

    algo(codeforces): My journey is just beginning (day61)

---
+++ codeforces/E_1999.cpp
@@ -0,0 +1,89 @@
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+
+//
+// Convenient IO functions.
+//
+
+template <typename... Args>
+void print(const Args &...args)
+{
+  (std::cout << ... << args);
+}
+
+template <typename... Args>
+void println(const Args &...args)
+{
+  (std::cout << ... << args) << "\n";
+}
+
+// ----- //
+
+const int UPPER_BOUND = 2 * 1e5;
+
+int exact_log_with_base(long num, int base)
+{
+  return std::log2(num) / std::log2(base);
+}
+
+auto precomputed_log_values = [](const long &range,
+                                 const int &base = 3) -> std::vector<long>
+{
+  static std::vector<long> needed_steps;
+  for (long num = 1; num <= range; num++)
+  {
+    int v = exact_log_with_base(num, base);
+    needed_steps.push_back(v + 1 /* 1 more step to reach 0 */);
+  }
+  return needed_steps;
+}(UPPER_BOUND);
+
+auto precomputed_list_sum_of_log = [](const std::vector<long> &computed_logs)
+    -> std::vector<long>
+{
+  static std::vector<long> list_sum_of_log;
+  int sum_val = 0;
+  for (auto &computed_log : computed_logs)
+  {
+    sum_val += computed_log;
+    list_sum_of_log.push_back(sum_val);
+  }
+  return list_sum_of_log;
+}(precomputed_log_values);
+
+int solve()
+{
+  unsigned long long l, r;
+  std::cin >> l >> r;
+
+  int smaller_sum = precomputed_list_sum_of_log[l - 1];
+  int greater_sum = precomputed_list_sum_of_log[r - 1];
+  return (greater_sum - smaller_sum) + precomputed_log_values[l - 1] * 2;
+}
+
+int main()
+{
+  std::ios_base::sync_with_stdio(false);
+  std::cin.tie(nullptr);
+
+  // auto test_log_vec = std::vector<int>(precomputed_log_values.begin(),
+  //                                      precomputed_log_values.begin() + 100);
+  // for (auto &v : test_log_vec)
+  //   println(v);
+
+  // auto test_sum_vec = std::vector<int>(precomputed_list_sum_of_log.begin(),
+  //                                      precomputed_list_sum_of_log.begin() + 10);
+  // for (auto &v : test_sum_vec)
+  //   println(v);
+
+  // println(precomputed_list_sum_of_log[199998]);
+  // println(precomputed_list_sum_of_log[199999]);
+
+  int T = 1;
+  std::cin >> T;
+  for (int i = 0; i < T; i++)
+    println(solve());
+
+  return 0;
+}
