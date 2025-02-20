c640978d5c9e7a77190acf8587d2eaf0c69ee60a
IMpcuong <trinhtiencuong_t63@hus.edu.vn>
2025-02-20 14:42:31 +0700

    algo(codeforces): My journey is just beginning (day64)

--- codeforces/D_1950.cpp
+++ codeforces/D_1950.cpp
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
 
-// clang++ -isystem . -std=c++20 -Wall -Wextra -O2 tmpl.cpp -o out -g
+// clang++ -isystem . -std=c++20 -Wall -Wextra -O2 tmpl.cpp -o out -v
 
 using namespace std;
 
@@ -51,6 +51,7 @@ void dbg_out(Head H, Tail... T)
 #endif
 
 #define ar array
+#define us unsigned shor
 #define ll long long
 #define ld long double
 #define sza(x) ((int)x.size())
@@ -61,10 +62,62 @@ const ll MOD = 1e9 + 7;
 const ll INF = 1e9;
 const ld EPS = 1e-9;
 
-void solve()
+const string Y = "YES";
+const string N = "NO";
+
+bool determine_binary_form_of_dec_num(int dec_num)
 {
-  vector v{1, 2, 3, 4, 5};
-  cout << v << " " << &v << "\n";
+  if (dec_num == 1)
+    return true;
+  while (dec_num > 0)
+  {
+    if (dec_num % 10 > 1)
+      return false;
+    dec_num /= 10;
+  }
+  return true;
+}
+
+auto binary_decimal_list = []() -> vector<int>
+{
+  vector<int> out;
+  out.reserve(MAX_N);
+  for (int num = 2; num < MAX_N; num++)
+  {
+    if (!determine_binary_form_of_dec_num(num))
+      continue;
+    out.emplace_back(num);
+  }
+  return out;
+}();
+
+bool is_product_by_only_binary_dec(int target)
+{
+  if (target == 1)
+    return true;
+
+  bool satisfied = false;
+  for (const auto &bin_decimal : binary_decimal_list)
+  {
+    if (target % bin_decimal == 0)
+    {
+      target /= bin_decimal;
+      satisfied |= is_product_by_only_binary_dec(target);
+    }
+  }
+  return satisfied;
+}
+
+string solve()
+{
+  int n;
+  cin >> n;
+
+  if (determine_binary_form_of_dec_num(n))
+    return Y;
+
+  bool valid = is_product_by_only_binary_dec(n);
+  return valid ? Y : N;
 }
 
 int main()
@@ -77,7 +130,7 @@ int main()
   cin >> tc;
   for (int t = 1; t <= tc; t++)
   {
-    cout << "Case #" << t << ": "; // @Warn: Commenting before submission.
-    solve();
+    // cout << "Case #" << t << ": "; // @Warn: Commenting before submission.
+    println(solve());
   }
 }
