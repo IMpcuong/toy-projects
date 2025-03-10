0771cfd54987f98145998c575c8aa6555798a63a
IMpcuong <trinhtiencuong_t63@hus.edu.vn>
2025-03-10 11:27:55 +0700

    algo(codeforces): My journey is just beginning (day72) (hardcore x2) (TUT)

---
+++ codeforces/C_2072.cpp
@@ -0,0 +1,119 @@
+#include <bits/stdc++.h>
+
+// clang++ -isystem . -std=c++20 -g -Wall -Wextra -O3 tmpl.cpp -o out
+
+using namespace std;
+
+template <typename A, typename B>
+ostream &operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+
+template <typename T_container,
+          typename T = typename enable_if<!is_same<T_container, string>::value,
+                                          typename T_container::value_type>::type>
+ostream &operator<<(ostream &os, const T_container &v)
+{
+  string sep;
+  for (const T &x : v)
+    os << sep << x, sep = " ";
+  return os;
+}
+
+template <typename... Args>
+void print(const Args &...args)
+{
+  bool end = true;
+  ((cout << (end ? (end = false, "") : " ") << args), ...);
+}
+
+template <typename... Args>
+void println(const Args &...args)
+{
+  bool end = true;
+  ((cout << (end ? (end = false, "") : " ") << args), ...) << "\n";
+}
+
+void dbg_out() { cerr << endl; }
+
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T)
+{
+  cerr << ' ' << H;
+  dbg_out(T...);
+}
+
+// #define LOCAL 1
+
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+#define ar array
+#define ll long long
+#define ld long double
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+inline int nxt()
+{
+  int x;
+  cin >> x;
+  return x;
+}
+
+void solve()
+{
+  int len = nxt();
+  int val = nxt();
+
+  //
+  // @Note:
+  //  + a | b >= max(a, b).
+  //  + a & b <= min(a, b).
+  //
+
+  vector<int> ans(len, val);
+  int or_val = 0;
+  bool flag = true;
+  for (int i = 0; i < len - 1; ++i)
+  {
+    int updated_val = or_val | i;
+    if ((updated_val & val) == updated_val)
+    {
+      or_val |= i;
+      ans[i] = i;
+    }
+    else
+    {
+      flag = false;
+      break;
+    }
+  }
+
+  if (flag && (or_val | (len - 1)) == val)
+    ans[len - 1] = len - 1;
+
+  cout << ans << "\n";
+}
+
+int main()
+{
+  ios_base::sync_with_stdio(0);
+  cin.exceptions(ios::badbit | ios::failbit);
+  cin.tie(0);
+  cout.tie(0);
+
+  int tc = 1;
+  cin >> tc;
+  for (int t = 1; t <= tc; t++)
+  {
+    // cout << "Case #" << t << ": "; // @Warn: Commenting before submission.
+    solve();
+  }
+}
