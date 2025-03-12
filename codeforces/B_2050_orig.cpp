a3e9ca8579f35a42440dd2efb673d1a6e0d13ef8
IMpcuong <trinhtiencuong_t63@hus.edu.vn>
2025-03-12 18:23:20 +0700

    algo(codeforces): My journey is just beginning (day74) (hardcore x3) (own idea)
    * @Note: std::accumulate (defines in <cmath>) initializes the accumulator
      (of type T) which coerced with the initial value `init`'s primitive type
      (number type in common sense).

---
+++ codeforces/B_2050_orig.cpp
@@ -0,0 +1,116 @@
+#include <bits/stdc++.h>
+
+// clang++ -isystem . -std=c++20 -g -Wall -Wextra -O3 tmpl.cpp -o out
+
+using namespace std;
+
+template <typename A, typename B>
+ostream &operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+
+template <typename T_container,
+          typename T = typename enable_if<!is_same<T_container, string>::value,
+                                          typename T_container::value_type>::type>
+ostream &operator<<(ostream &os, const T_container &v)
+{
+  os << '{';
+  string sep;
+  for (const T &x : v)
+    os << sep << x, sep = ", ";
+  return os << '}';
+}
+
+template <typename... Args>
+void print(const Args &...args)
+{
+  bool end = true;
+  ((cout << (end ? (end = false, "") : " ") << args), ...);
+}
+
+template <typename... Args>
+void println(const Args &...args)
+{
+  bool end = true;
+  ((cout << (end ? (end = false, "") : " ") << args), ...) << "\n";
+}
+
+void dbg_out() { cerr << endl; }
+
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T)
+{
+  cerr << ' ' << H;
+  dbg_out(T...);
+}
+
+// #define LOCAL 1
+
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+#define ar array
+#define ll long long
+#define ld long double
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+constexpr string _Y = "YES";
+constexpr string _N = "NO";
+
+template <typename T>
+inline T nxt()
+{
+  T x;
+  cin >> x;
+  return x;
+}
+
+string solve()
+{
+  auto len = nxt<int>();
+  vector<int> a(len);
+  generate(all(a), nxt<int>);
+
+  ll sum = accumulate(all(a), 0LL);
+  if (sum % len != 0)
+    return _N;
+
+  int base_val = sum / len;
+  for (int i = 1; i < len - 1; i++)
+  {
+    int l = a[i - 1];
+    if (l == base_val)
+      continue;
+    
+    a[i - 1] = base_val;
+    if (l < base_val)
+      a[i + 1] -= (base_val - l);
+    if (l > base_val)
+      a[i + 1] += (l - base_val);
+  }
+
+  return ranges::count(all(a), base_val) == len ? _Y : _N;
+}
+
+int main()
+{
+  ios_base::sync_with_stdio(0);
+  cin.exceptions(ios::badbit | ios::failbit);
+  cin.tie(0);
+  cout.tie(0);
+
+  int tc = 1;
+  cin >> tc;
+  for (int t = 1; t <= tc; t++)
+  {
+    // cout << "Case #" << t << ": "; // @Warn: Commenting before submission.
+    println(solve());
+  }
+}
