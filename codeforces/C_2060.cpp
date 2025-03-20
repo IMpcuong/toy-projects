4f17b9fdeff656efdadfc8f716b20c140a2575fa
IMpcuong <trinhtiencuong_t63@hus.edu.vn>
2025-03-20 16:50:52 +0700

    algo(codeforces): My journey is just beginning (day81) (hardcore) (TUT)

---
+++ codeforces/C_2060.cpp
@@ -0,0 +1,121 @@
+#include <bits/stdc++.h>
+
+// clang++ -isystem . -std=c++20 -g -Wall -Wextra -O3 tmpl.cpp -o out
+
+using namespace std;
+
+template <typename A, typename B>
+ostream &operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+
+template <typename T_container,
+          typename T = typename enable_if<!is_same<T_container, string>::value,
+                                          typename T_container::value_type>::type>
+ostream &operator<<(ostream &os, const T_container &v)
+{
+  os << '{';
+  string sep;
+  for (const T &x : v)
+    os << sep << x, sep = ", ";
+  return os << '}';
+}
+
+template <typename... Args>
+void print(const Args &...args)
+{
+  bool end = true;
+  ((cout << (end ? (end = false, "") : " ") << args), ...);
+}
+
+template <typename... Args>
+void println(const Args &...args)
+{
+  bool end = true;
+  ((cout << (end ? (end = false, "") : " ") << args), ...) << "\n";
+}
+
+void dbg_out() { cerr << endl; }
+
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T)
+{
+  cerr << ' ' << H;
+  dbg_out(T...);
+}
+
+// #define LOCAL 1
+
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+#define ar array
+#define ll long long
+#define ld long double
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+template <typename T>
+inline T nxt()
+{
+  T x;
+  cin >> x;
+  return x;
+}
+
+void solve()
+{
+  auto n = nxt<int>();
+  auto k = nxt<int>();
+  vector<int> a(n);
+  generate(all(a), nxt<int>);
+  sort(all(a));
+
+  // @Note: freq[1] -> freq[k].
+  vector<int> freq(k + 1, 0);
+  for (const auto &num : a)
+  {
+    if (num >= k)
+      continue;
+    freq[num]++;
+  }
+
+  int score = 0;
+  for (int i = 1; i < k; i++)
+  {
+    if (i == k - i)
+    {
+      score += freq[i] / 2;
+      continue;
+    }
+
+    int less_occurs = min(freq[i], freq[k - i]);
+    score += less_occurs;
+    freq[i] -= less_occurs;
+    freq[k - i] -= less_occurs;
+  }
+
+  println(score);
+}
+
+int main()
+{
+  ios_base::sync_with_stdio(0);
+  cin.exceptions(ios::badbit | ios::failbit);
+  cin.tie(0);
+  cout.tie(0);
+
+  int tc = 1;
+  cin >> tc;
+  for (int t = 1; t <= tc; t++)
+  {
+    // cout << "Case #" << t << ": "; // @Warn: Commenting before submission.
+    solve();
+  }
+}
