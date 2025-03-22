4701c3e63f73d202354da5242cb3a8dbfc5a4364
IMpcuong <cuongtrinhtien128@gmail.com>
2025-03-22 18:11:08 +0700

    algo(codeforces): My journey is just beginning (day83) (hardcore) (WIP)

--- codeforces/C_1899.cpp
+++ codeforces/C_1899.cpp
@@ -74,6 +74,54 @@ void solve()
   auto n = nxt<long>();
   vector<int> a(n);
   generate(all(a), nxt<int>);
+
+  vector<long> inc_sum = {0};
+  inc_sum.reserve(n + 1);
+  for (const auto &v : a)
+    inc_sum.emplace_back(inc_sum.back() + v);
+
+  vector<pair<int, int>> range_pairs;
+  range_pairs.reserve(n);
+  int start = 0;
+  int end = 0;
+  for (int i = 1; i < n; i++)
+  {
+    int prev = a[i - 1];
+    int cur = a[i];
+    if ((cur + prev) & 1)
+    {
+      end = i;
+    }
+    else
+    {
+      if (end > start)
+        range_pairs.emplace_back(start, end);
+      start = i;
+      end = i;
+    }
+  }
+  if (end > start)
+    range_pairs.emplace_back(start, end);
+  if (end == n - 1 && start == n - 1)
+    for (int i = 0; i < n; i++)
+      range_pairs.emplace_back(i, i);
+  range_pairs.shrink_to_fit();
+
+  // cout << range_pairs << "\n";
+
+  long max_sum = -INF;
+  for (auto const &p : range_pairs)
+  {
+    long cur_sum = 0;
+    for (int i = p.first; i <= p.second; i++)
+    {
+      cur_sum = max(cur_sum + long(a[i]), long(a[i]));
+      max_sum = max(max_sum, cur_sum);
+      println(cur_sum, max_sum);
+    }
+  }
+
+  println(max_sum);
 }
 
 int main()
