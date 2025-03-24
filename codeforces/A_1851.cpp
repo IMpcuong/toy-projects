e2a1335ee15475e3541f526551bc71ab2b326a2a
IMpcuong <trinhtiencuong_t63@hus.edu.vn>
2025-03-24 15:29:30 +0700

    algo(codeforces): My journey is just beginning (day85) (hardcore)

---
+++ codeforces/A_1851.cpp
@@ -0,0 +1,111 @@
+#include <bits/stdc++.h>
+#include <ostream>
+
+// clang++ -isystem . -std=c++20 -g -Wall -Wextra -O3 tmpl.cpp -o out
+
+using namespace std;
+
+template <typename A, typename B>
+ostream &operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+
+template <typename T_container,
+          typename T = typename enable_if<!is_same<T_container, string>::value,
+                                          typename T_container::value_type>::type>
+ostream &operator<<(ostream &os, const T_container &v)
+{
+  os << '{';
+  string sep;
+  for (const T &x : v)
+    os << sep << x, sep = ", ";
+  return os << '}';
+}
+
+template <typename... Args>
+void print(const Args &...args)
+{
+  bool end = true;
+  ((cout << (end ? (end = false, "") : " ") << args), ...);
+}
+
+template <typename... Args>
+void println(const Args &...args)
+{
+  bool end = true;
+  ((cout << (end ? (end = false, "") : " ") << args), ...) << "\n";
+}
+
+void dbg_out() { cerr << endl; }
+
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T)
+{
+  cerr << ' ' << H;
+  dbg_out(T...);
+}
+
+// #define LOCAL 1
+
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+#define ar array
+#define ll long long
+#define ld long double
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+template <typename T>
+inline T nxt()
+{
+  T x;
+  cin >> x;
+  return x;
+}
+
+void solve()
+{
+  auto n = nxt<int>();
+  auto steps = nxt<int>();
+  auto k = nxt<long>();
+  auto vlad_h = nxt<long>();
+  vector<long> h(n);
+  generate(all(h), nxt<long>);
+
+  long cnt = 0;
+  for (int i = 0; i < n; i++)
+  {
+    long diff_h = abs(h[i] - vlad_h);
+    if (diff_h % k != 0)
+      continue;
+
+    int possible_step = static_cast<int>(diff_h / k);
+    if (possible_step >= 1 && possible_step < steps)
+      cnt++;
+  }
+
+  println(cnt);
+}
+
+int main()
+{
+  ios_base::sync_with_stdio(0);
+  cin.exceptions(ios::badbit | ios::failbit);
+  cin.tie(0);
+  cout.tie(0);
+
+  int tc = 1;
+  cin >> tc;
+  for (int t = 1; t <= tc; t++)
+  {
+    // cout << "Case #" << t << ": "; // @Warn: Commenting before submission.
+    solve();
+  }
+}
