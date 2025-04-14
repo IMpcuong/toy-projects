2e71071f4362773ad7ca658f93960125c1b12371
IMpcuong <trinhtiencuong_t63@hus.edu.vn>
2025-04-14 17:20:15 +0700

    algo(codeforces): My journey is just beginning (day99) (hardcore x2)

---
+++ codeforces/C_1311.cpp
@@ -0,0 +1,124 @@
+#include <bits/stdc++.h>
+
+// clang++ -isystem . -std=c++20 -g -Wall -Wextra -O3 tmpl.cpp -o out
+
+using namespace std;
+
+template <typename A, typename B>
+ostream &operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+
+template <typename T_container,
+          typename T = typename enable_if<!is_same<T_container, string>::value,
+                                          typename T_container::value_type>::type>
+ostream &operator<<(ostream &os, const T_container &v)
+{
+  string sep;
+  for (const T &x : v)
+    os << sep << x, sep = " ";
+  return os;
+}
+
+template <typename... Args>
+void print(const Args &...args)
+{
+  bool end = true;
+  ((cout << (end ? (end = false, "") : " ") << args), ...);
+}
+
+template <typename... Args>
+void println(const Args &...args)
+{
+  bool end = true;
+  ((cout << (end ? (end = false, "") : " ") << args), ...) << "\n";
+}
+
+void dbg_out() { cerr << endl; }
+
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T)
+{
+  cerr << ' ' << H;
+  dbg_out(T...);
+}
+
+// #define LOCAL 1
+
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+#define ar array
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+#define prec(k) cout << fixed << setprecision(k);
+#define ceildiv(n, k) (((n) + (k) - 1) / (k))
+
+using ll = long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using point = complex<double>;
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+inline ll gcd(ll a, ll b) { return b == 0 ? a : gcd(b, a % b); }
+inline ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+
+template <typename T>
+inline T nxt()
+{
+  T x;
+  cin >> x;
+  return x;
+}
+
+void solve()
+{
+  auto n = nxt<int>();
+  auto tries = nxt<int>();
+  auto combo = nxt<string>();
+  vector<int> fails(tries);
+  generate(all(fails), nxt<int>);
+  ranges::sort(fails);
+
+  vector<int> freq(26);
+  for (const auto &chr : combo)
+    freq[static_cast<int>(chr) - 97]++;
+
+  int upper = *max_element(all(fails));
+  vector<ar<int, 26>> pref_sum(upper + 2);
+  pref_sum[0].fill(0);
+  for (int turn = 0; turn < upper + 1; turn++)
+  {
+    int chr_idx = static_cast<int>(combo[turn]) - 97;
+    pref_sum[turn + 1] = pref_sum[turn];
+    pref_sum[turn + 1][chr_idx]++;
+  }
+
+  for (const auto &turn : fails)
+    for (int i = 0; i < 26; i++)
+      freq[i] += pref_sum[turn][i];
+
+  cout << freq << "\n";
+}
+
+int main()
+{
+  ios_base::sync_with_stdio(0);
+  cin.exceptions(ios::badbit | ios::failbit);
+  cin.tie(0);
+  cout.tie(0);
+
+  int tc = 1;
+  cin >> tc;
+  for (int t = 1; t <= tc; t++)
+  {
+    // cout << "Case #" << t << ": "; // @Warn: Commenting before submission.
+    solve();
+  }
+}
