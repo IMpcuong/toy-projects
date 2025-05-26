f33cb88f21c44c9e30ec7d8fa4542996cae02487
IMpcuong <trinhtiencuong_t63@hus.edu.vn>
2025-05-26 19:46:33 +0700

    algo(codeforces): My journey is just beginning (day119) (TLE)

---
+++ codeforces/C_1744.cpp
@@ -0,0 +1,138 @@
+#include <bits/stdc++.h>
+
+// clang++ -isystem . -std=c++20 -g -Wall -Wextra -O3 tmpl.cpp -o out
+
+using namespace std;
+
+template <typename A, typename B>
+ostream &operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+
+template <typename T_container,
+          typename T = typename enable_if<!is_same<T_container, string>::value,
+                                          typename T_container::value_type>::type>
+ostream &operator<<(ostream &os, const T_container &v)
+{
+  os << '{';
+  string sep;
+  for (const T &x : v)
+    os << sep << x, sep = ", ";
+  return os << '}';
+}
+
+template <typename... Args>
+void print(const Args &...args)
+{
+  bool end = true;
+  ((cout << (end ? (end = false, "") : " ") << args), ...);
+}
+
+template <typename... Args>
+void println(const Args &...args)
+{
+  bool end = true;
+  ((cout << (end ? (end = false, "") : " ") << args), ...) << "\n";
+}
+
+void dbg_out() { cerr << endl; }
+
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T)
+{
+  cerr << ' ' << H;
+  dbg_out(T...);
+}
+
+// #define LOCAL 1
+
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+#define ar array
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+#define prec(k) cout << fixed << setprecision(k);
+#define ceildiv(n, k) (((n) + (k) - 1) / (k))
+
+using ll = long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using point = complex<double>;
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+inline ll gcd(ll a, ll b) { return b == 0 ? a : gcd(b, a % b); }
+inline ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+
+template <typename T>
+inline T nxt()
+{
+  T x;
+  cin >> x;
+  return x;
+}
+
+void solve()
+{
+  auto n = nxt<int>();
+  auto cur_color = nxt<char>();
+  auto traffic_lights = nxt<string>();
+  traffic_lights += traffic_lights; // cyclical.
+
+  if (n == 1)
+  {
+    println(0);
+    return;
+  }
+
+  vector<int> cur_color_locs;
+  cur_color_locs.reserve(n);
+  for (int i = 0; i < n; i++)
+    if (traffic_lights[i] == cur_color)
+      cur_color_locs.emplace_back(i);
+  cur_color_locs.shrink_to_fit();
+
+  int cur_color_cnt = sza(cur_color_locs);
+  int guarantee_green_dist = -1;
+  int j1 = 0;
+  for (int j = 0; j < cur_color_cnt; j++)
+  {
+    int cur = cur_color_locs[j];
+    j1 = cur;
+
+    while (j1 < n + cur)
+    {
+      if (traffic_lights[j1] == 'g')
+      {
+        guarantee_green_dist = max(guarantee_green_dist, j1 - cur);
+        break;
+      }
+
+      j1++;
+    }
+  }
+
+  println(guarantee_green_dist);
+}
+
+int main()
+{
+  ios_base::sync_with_stdio(0);
+  cin.exceptions(ios::badbit | ios::failbit);
+  cin.tie(0);
+  cout.tie(0);
+
+  int tc = 1;
+  cin >> tc;
+  for (int t = 1; t <= tc; t++)
+  {
+    // cout << "Case #" << t << ": "; // @Warn: Commenting before submission.
+    solve();
+  }
+}
