a025187d7831cbc2ada6b0b3f498add124a6823c
IMpcuong <trinhtiencuong_t63@hus.edu.vn>
2025-05-28 16:45:15 +0700

    learn(asm): Memory access ref table

--- asm-the-hardway/add.asm
+++ asm-the-hardway/add.asm
@@ -4,6 +4,13 @@
 ;  + .bss: read-write + zero-init
 ;  + .text: readonly + program code
 
+; Memory access:
+;  | Access memory  | Allocate memory |
+;  | byte  [ptr]    | db              |
+;  | word  [ptr]    | dw              |
+;  | dword [ptr]    | dd              |
+;  | qword [ptr]    | dq              |
+
 ; @Doc: https://forum.nasm.us/index.php?topic=1932.0
 
 default rel
@@ -18,9 +25,10 @@ start:
   mov  rax, qword [var1]
   mov  rbx, qword [var2]
   add  rax, rbx
+  shr  rax, 2
   mov  rdi, rax
-  mov  rax, 0x2000001
-  ; rax := 600
-  ; shel exit-codes := [i | 0 <= i <= 255]
-  ; echo $? := 600 % 256 = 88
+  mov  rax, 0x02000001
+  ; rax := 600 >> 2 = 150
+  ; shell exit-codes := [i | 0 <= i <= 255]
+  ; echo $? := 600 % 256 = 88 -> 150
   syscall
--- asm-the-hardway/hello_x64.asm
+++ asm-the-hardway/hello_x64.asm
@@ -11,19 +11,18 @@
 ;     ./a.out
 ; ----------------------------------------------------------------------------------------
 
-  global    start
+section .data
+message: db "Hello, World", 10 ; note the newline at the end
 
-  section   .text
+section .text
+global  start
 start:
-  mov       rax, 0x02000004         ; system call for write
-  mov       rdi, 1                  ; file handle 1 is stdout
-  mov       rsi, message            ; address of string to output
-  mov       rdx, 13                 ; number of bytes
-  syscall                           ; invoke operating system to do the write
-  mov       rax, 0x02000001         ; system call for exit
-  xor       rdi, rdi                ; exit code 0
-  syscall                           ; invoke operating system to exit
+  mov  rax, 0x02000004         ; system call for write
+  mov  rdi, 1                  ; file handle 1 is stdout
+  mov  rsi, message            ; address of string to output
+  mov  rdx, 13                 ; number of bytes
+  syscall                      ; invoke operating system to do the write
 
-  section   .data
-message:
-  db        "Hello, World", 10      ; note the newline at the end
+  mov  rax, 0x02000001         ; system call for exit
+  xor  rdi, rdi                ; exit code 0
+  syscall                      ; invoke operating system to exit
