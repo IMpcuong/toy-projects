4430817b8e328c689787399b28b3120bc7c7d1a9
IMpcuong <trinhtiencuong_t63@hus.edu.vn>
2025-07-24 18:11:00 +0700

    algo(codeforces): My journey is just beginning (day158) (harcore x1)

---
+++ codeforces/C_1741.cpp
@@ -0,0 +1,148 @@
+#include <bits/stdc++.h>
+
+// clang++ -isystem . -std=c++20 -g -Wall -Wextra -O3 tmpl.cpp -o out
+
+using namespace std;
+
+template <typename A, typename B>
+ostream &operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+
+template <typename T_container,
+          typename T = typename enable_if<!is_same<T_container, string>::value,
+                                          typename T_container::value_type>::type>
+ostream &operator<<(ostream &os, const T_container &v)
+{
+  os << '{';
+  string sep;
+  for (const T &x : v)
+    os << sep << x, sep = ", ";
+  return os << '}';
+}
+
+template <typename... Args>
+void print(const Args &...args)
+{
+  bool end = true;
+  ((cout << (end ? (end = false, "") : " ") << args), ...);
+}
+
+template <typename... Args>
+void println(const Args &...args)
+{
+  bool end = true;
+  ((cout << (end ? (end = false, "") : " ") << args), ...) << "\n";
+}
+
+void dbg_out() { cerr << endl; }
+
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T)
+{
+  cerr << ' ' << H;
+  dbg_out(T...);
+}
+
+// #define LOCAL 1
+
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+#define ar array
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+#define prec(k) cout << fixed << setprecision(k);
+#define ceildiv(n, k) (((n) + (k) - 1) / (k))
+
+using ll = long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using point = complex<double>;
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+inline ll gcd(ll a, ll b) { return b == 0 ? a : gcd(b, a % b); }
+inline ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+
+template <typename T>
+inline T nxt()
+{
+  T x;
+  cin >> x;
+  return x;
+}
+
+void solve()
+{
+  auto n = nxt<int>();
+  vector<ll> a(n);
+  ranges::generate(a, nxt<ll>);
+
+  ll min_segment_v = *ranges::min_element(a);
+  ll max_segment_v = accumulate(all(a), 0LL);
+  ll sqrt_max = static_cast<ll>(sqrt(double(max_segment_v)));
+
+  set<ll> divisors;
+  for (int i = 1; i <= sqrt_max; i++)
+  {
+    if (max_segment_v % i)
+      continue;
+
+    divisors.emplace(i);
+    divisors.emplace(max_segment_v / i);
+  }
+
+  int thickness = n;
+  for (const auto &div_sum : divisors)
+  {
+    int cur_thickness = 0;
+    int j = 0;
+    ll segment_sum = 0;
+    ll divisible_to_equal_chunks = true;
+    for (int i = 0; i < n; i++)
+    {
+      while (segment_sum < div_sum)
+      {
+        segment_sum += a[j];
+        j++;
+      }
+
+      if (segment_sum > div_sum)
+      {
+        divisible_to_equal_chunks = false;
+        break;
+      }
+
+      cur_thickness = j - i + 1;
+      segment_sum = 0;
+      i = j;
+    }
+
+    if (divisible_to_equal_chunks)
+      thickness = min(thickness, cur_thickness);
+  }
+
+  println(thickness);
+}
+
+int main()
+{
+  ios_base::sync_with_stdio(0);
+  cin.exceptions(ios::badbit | ios::failbit);
+  cin.tie(0);
+  cout.tie(0);
+
+  int tc = 1;
+  cin >> tc;
+  for (int t = 1; t <= tc; t++)
+  {
+    // cout << "Case #" << t << ": "; // @Warn: Commenting before submission.
+    solve();
+  }
+}
