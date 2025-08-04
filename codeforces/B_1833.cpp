67469eed588d3826685f89858c30441eafe1a8d5
IMpcuong <trinhtiencuong_t63@hus.edu.vn>
2025-08-04 13:36:31 +0700

    algo(codeforces): My journey is just beginning (day166) (hardcore x2)

---
+++ codeforces/B_1833.cpp
@@ -0,0 +1,123 @@
+#include <bits/stdc++.h>
+
+// clang++ -isystem . -std=c++20 -g -Wall -Wextra -O3 tmpl.cpp -o out
+
+using namespace std;
+
+template <typename A, typename B>
+ostream &operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+
+template <typename T_container,
+          typename T = typename enable_if<!is_same<T_container, string>::value,
+                                          typename T_container::value_type>::type>
+ostream &operator<<(ostream &os, const T_container &v)
+{
+  string sep;
+  for (const T &x : v)
+    os << sep << x, sep = " ";
+  return os;
+}
+
+template <typename... Args>
+void print(const Args &...args)
+{
+  bool end = true;
+  ((cout << (end ? (end = false, "") : " ") << args), ...);
+}
+
+template <typename... Args>
+void println(const Args &...args)
+{
+  bool end = true;
+  ((cout << (end ? (end = false, "") : " ") << args), ...) << "\n";
+}
+
+void dbg_out() { cerr << endl; }
+
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T)
+{
+  cerr << ' ' << H;
+  dbg_out(T...);
+}
+
+// #define LOCAL 1
+
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+#define ar array
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+#define prec(k) cout << fixed << setprecision(k);
+#define ceildiv(n, k) (((n) + (k) - 1) / (k))
+
+using ll = long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using point = complex<double>;
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+inline ll gcd(ll a, ll b) { return b == 0 ? a : gcd(b, a % b); }
+inline ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+
+template <typename T>
+inline T nxt()
+{
+  T x;
+  cin >> x;
+  return x;
+}
+
+void solve()
+{
+  auto n = nxt<int>();
+  auto max_delta = nxt<ll>();
+  vector<ll> estimate_degrees(n);
+  ranges::generate(estimate_degrees, nxt<ll>);
+  vector<ll> actual_degrees(n);
+  ranges::generate(actual_degrees, nxt<ll>);
+
+  vector<pair<ll, int>> estimate_degrees_in_day(n);
+  for (int i = 0; i < n; i++)
+    estimate_degrees_in_day[i] = make_pair(estimate_degrees[i], i);
+
+  ranges::sort(estimate_degrees_in_day,
+      [](const auto &temp1, const auto &temp2) { return temp1.first < temp2.first; });
+  ranges::sort(actual_degrees);
+
+  for (int i = 0; i < n; i++)
+    estimate_degrees_in_day[i].first = actual_degrees[i];
+  ranges::sort(estimate_degrees_in_day,
+      [](const auto &temp1, const auto &temp2) { return temp1.second < temp2.second; });
+
+  vector<ll> ans(n);
+  for (int i = 0; i < n; i++)
+    ans[i] = estimate_degrees_in_day[i].first;
+
+  cout << ans << "\n";
+}
+
+int main()
+{
+  ios_base::sync_with_stdio(0);
+  cin.exceptions(ios::badbit | ios::failbit);
+  cin.tie(0);
+  cout.tie(0);
+
+  int tc = 1;
+  cin >> tc;
+  for (int t = 1; t <= tc; t++)
+  {
+    // cout << "Case #" << t << ": "; // @Warn: Commenting before submission.
+    solve();
+  }
+}
