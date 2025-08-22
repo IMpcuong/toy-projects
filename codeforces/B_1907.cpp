899690674e9444be6f7c24a82756b02899d95d62
IMpcuong <trinhtiencuong_t63@hus.edu.vn>
2025-08-22 18:25:02 +0700

    algo(codeforces): My journey is just beginning (day178) (hardcore x3)

---
+++ codeforces/B_1907.cpp
@@ -0,0 +1,141 @@
+#include <bits/stdc++.h>
+#include <cctype>
+
+// clang++ -isystem . -std=c++20 -g -Wall -Wextra -O3 tmpl.cpp -o out
+
+using namespace std;
+
+template <typename A, typename B>
+ostream &operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+
+template <typename T_container,
+          typename T = typename enable_if<!is_same<T_container, string>::value,
+                                          typename T_container::value_type>::type>
+ostream &operator<<(ostream &os, const T_container &v)
+{
+  os << '{';
+  string sep;
+  for (const T &x : v)
+    os << sep << x, sep = ", ";
+  return os << '}';
+}
+
+template <typename... Args>
+void print(const Args &...args)
+{
+  bool end = true;
+  ((cout << (end ? (end = false, "") : " ") << args), ...);
+}
+
+template <typename... Args>
+void println(const Args &...args)
+{
+  bool end = true;
+  ((cout << (end ? (end = false, "") : " ") << args), ...) << "\n";
+}
+
+void dbg_out() { cerr << endl; }
+
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T)
+{
+  cerr << ' ' << H;
+  dbg_out(T...);
+}
+
+// #define LOCAL 1
+
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+#define ar array
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+#define prec(k) cout << fixed << setprecision(k);
+#define ceildiv(n, k) (((n) + (k) - 1) / (k))
+
+using ll = long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using point = complex<double>;
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+inline ll gcd(ll a, ll b) { return b == 0 ? a : gcd(b, a % b); }
+inline ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+
+template <typename T>
+inline T nxt()
+{
+  T x;
+  cin >> x;
+  return x;
+}
+
+void solve()
+{
+  auto s = nxt<string>();
+
+  int sz = sza(s);
+  stack<int> st_lower;
+  stack<int> st_upper;
+  for (int i = 0; i < sz; i++)
+  {
+    char cur = s[i];
+    if (cur != 'b' && cur != 'B')
+    {
+      if (islower(cur))
+        st_lower.push(i);
+      if (isupper(cur))
+        st_upper.push(i);
+      continue;
+    }
+
+    if (cur == 'b' && !st_lower.empty())
+      st_lower.pop();
+    if (cur == 'B' && !st_upper.empty())
+      st_upper.pop();
+  }
+
+  vector<bool> remains(sz, false);
+  auto fill_vec = [&](stack<int> &st) -> void
+  {
+    while (!st.empty())
+    {
+      remains[st.top()] = true;
+      st.pop();
+    }
+  };
+
+  fill_vec(st_lower);
+  fill_vec(st_upper);
+  string ans = "";
+  for (int i = 0; i < sz; i++)
+    if (remains[i])
+      ans += s[i];
+
+  println(ans);
+}
+
+int main()
+{
+  ios_base::sync_with_stdio(0);
+  cin.exceptions(ios::badbit | ios::failbit);
+  cin.tie(0);
+  cout.tie(0);
+
+  int tc = 1;
+  cin >> tc;
+  for (int t = 1; t <= tc; t++)
+  {
+    // cout << "Case #" << t << ": "; // @Warn: Commenting before submission.
+    solve();
+  }
+}
