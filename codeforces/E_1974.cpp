7b4564d0c3d2b27d9876e861411e69bb7ff065ab
IMpcuong <trinhtiencuong_t63@hus.edu.vn>
2025-10-09 17:24:27 +0700

    algo(codeforces): My journey is just beginning (day200) (hardcore x1) (TUT)

---
+++ codeforces/E_1974.cpp
@@ -0,0 +1,151 @@
+#include <bits/stdc++.h>
+
+// clang++ -isystem . -std=c++20 -g -Wall -Wextra -fsanitize=address -O3 tmpl.cpp -o out
+
+using namespace std;
+
+template <typename A, typename B>
+ostream &operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+
+template <typename ...Args>
+ostream &operator<<(ostream &os, const tuple<Args...> &t)
+{
+  os << '{';
+  string sep;
+  apply([&](auto &&...args) { ((os << sep << args, sep = ", "), ...); }, t);
+  return os << '}';
+}
+
+template <typename T_container,
+          typename T = typename enable_if<!is_same<T_container, string>::value,
+                                          typename T_container::value_type>::type>
+ostream &operator<<(ostream &os, const T_container &v)
+{
+  os << '{';
+  string sep;
+  for (const T &x : v)
+    os << sep << x, sep = ", ";
+  return os << '}';
+}
+
+template <typename... Args>
+void print(const Args &...args)
+{
+  bool end = true;
+  ((cout << (end ? (end = false, "") : " ") << args), ...);
+}
+
+template <typename... Args>
+void println(const Args &...args)
+{
+  bool end = true;
+  ((cout << (end ? (end = false, "") : " ") << args), ...) << "\n";
+}
+
+void dbg_out() { cerr << endl; }
+
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T)
+{
+  cerr << ' ' << H;
+  dbg_out(T...);
+}
+
+// #define LOCAL 1
+
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+#define ar array
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+#define prec(k) cout << fixed << setprecision(k);
+#define ceildiv(n, k) (((n) + (k) - 1) / (k))
+
+using ll = long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using point = complex<double>;
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+inline ll gcd(ll a, ll b) { return b == 0 ? a : gcd(b, a % b); }
+inline ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+
+template <typename T>
+inline T nxt()
+{
+  T x;
+  cin >> x;
+  return x;
+}
+
+const ll OVER_INF = 1e18;
+
+void solve()
+{
+  auto month_cnt = nxt<int>();
+  auto wage = nxt<ll>();
+  vector<ll> costs(month_cnt);
+  vector<int> values(month_cnt);
+  for (int i = 0; i < month_cnt; i++)
+  {
+    auto c = nxt<ll>();
+    auto v = nxt<int>();
+
+    costs[i]  = c;
+    values[i] = v;
+  }
+  int max_v = accumulate(all(values), 0);
+
+  //
+  // @Note:
+  //  + 0 <= j <= max_v
+  //  + dp[j] := minimum cost to achieve value j
+  //
+  vector<ll> dp(max_v + 1, OVER_INF);
+  dp[0] = 0;
+  for (int i = 0; i < month_cnt; i++)
+  {
+    ll cur_c = costs[i];
+    int cur_v = values[i];
+    for (int j = max_v; j >= cur_v; j--)
+    {
+      ll possible_c = dp[j - cur_v] + cur_c;
+      if (possible_c <= ll(i) * wage)
+        dp[j] = min(dp[j], possible_c);
+    }
+  }
+
+  for (int j = max_v; j >= 0; j--)
+  {
+    if (dp[j] < OVER_INF)
+    {
+      println(j);
+      break;
+    }
+  }
+}
+
+int main()
+{
+  ios_base::sync_with_stdio(0);
+  cin.exceptions(ios::badbit | ios::failbit);
+  cin.tie(0);
+  cout.tie(0);
+
+  int tc = 1;
+  cin >> tc;
+  for (int t = 1; t <= tc; t++)
+  {
+    // cout << "Case #" << t << ": "; // @Warn: Commenting before submission.
+    solve();
+  }
+}
