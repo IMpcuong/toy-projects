92ab02693a0b8cafa71bbd435d1aa8cbde074a9b
IMpcuong <trinhtiencuong_t63@hus.edu.vn>
2025-11-04 15:17:13 +0700

    algo(cses): My journey is just beginning (day216) (hardcore x1)

---
+++ codeforces/CSES_1158.cpp
@@ -0,0 +1,134 @@
+#include <bits/stdc++.h>
+
+// clang++ -isystem . -std=c++20 -g -Wall -Wextra -fsanitize=address -O3 tmpl.cpp -o out
+
+using namespace std;
+
+template <typename A, typename B>
+ostream &operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+
+template <typename ...Args>
+ostream &operator<<(ostream &os, const tuple<Args...> &t)
+{
+  os << '{';
+  string sep;
+  apply([&](auto &&...args) { ((os << sep << args, sep = ", "), ...); }, t);
+  return os << '}';
+}
+
+template <typename T_container,
+          typename T = typename enable_if<!is_same<T_container, string>::value,
+                                                   typename T_container::value_type>::type>
+ostream &operator<<(ostream &os, const T_container &v)
+{
+  os << '{';
+  string sep;
+  for (const T &x : v)
+    os << sep << x, sep = ", ";
+  return os << '}';
+}
+
+template <typename... Args>
+void print(const Args &...args)
+{
+  bool end = true;
+  ((cout << (end ? (end = false, "") : " ") << args), ...);
+}
+
+template <typename... Args>
+void println(const Args &...args)
+{
+  bool end = true;
+  ((cout << (end ? (end = false, "") : " ") << args), ...) << "\n";
+}
+
+void dbg_out() { cerr << endl; }
+
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T)
+{
+  cerr << ' ' << H;
+  dbg_out(T...);
+}
+
+// #define LOCAL 1
+
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+#define ar array
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+#define prec(k) cout << fixed << setprecision(k);
+#define ceildiv(n, k) (((n) + (k) - 1) / (k))
+
+using ll = long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using point = complex<double>;
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+inline ll gcd(ll a, ll b) { return b == 0 ? a : gcd(b, a % b); }
+inline ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+
+template <typename T>
+inline T nxt()
+{
+  T x;
+  cin >> x;
+  return x;
+}
+
+void solve()
+{
+  auto book_quan = nxt<int>();
+  auto total_spent = nxt<int>();
+  vector<int> prices(book_quan);
+  ranges::generate(prices, nxt<int>);
+  vector<int> pages(book_quan);
+  ranges::generate(pages, nxt<int>);
+
+  vector<vector<int>> dp(book_quan + 1, vector<int>(total_spent + 1, 0L));
+  for (int i = 1; i < book_quan + 1; i++)
+  {
+    int cur_price = prices[i - 1];
+    int cur_page  = pages[i - 1];
+    for (int paid = 1; paid < total_spent + 1; paid++)
+    {
+      // Ignores `books[i]`:
+      dp[i][paid] = dp[i - 1][paid];
+
+      // Buys `books[i]`:
+      if (cur_price > paid)
+        continue;
+      int page_quan_if_buy = dp[i - 1][paid - cur_price] + cur_page;
+      dp[i][paid] = max(dp[i][paid], page_quan_if_buy);
+    }
+  }
+
+  println(dp[book_quan][total_spent]);
+}
+
+int main()
+{
+  ios_base::sync_with_stdio(0);
+  cin.exceptions(ios::badbit | ios::failbit);
+  cin.tie(0);
+  cout.tie(0);
+
+  int tc = 1;
+  // cin >> tc;
+  for (int t = 1; t <= tc; t++)
+  {
+    // cout << "Case #" << t << ": "; // @Warn: Commenting before submission.
+    solve();
+  }
+}
