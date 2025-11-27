e3a3c62fe9d38d8b32e2779e4c9293395433eb7a
IMpcuong <trinhtiencuong_t63@hus.edu.vn>
2025-11-27 19:08:49 +0700

    algo(codeforces): My journey is just beginning (day225) (hardcore x4)

---
+++ codeforces/B_2057.cpp
@@ -0,0 +1,155 @@
+#include <bits/stdc++.h>
+
+// clang++ -isystem . -std=c++20 -g -Wall -Wextra -fsanitize=address -O3 tmpl.cpp -o out
+
+using namespace std;
+
+template <typename A, typename B>
+ostream &operator<<(ostream &os, const pair<A, B> &p) { return os << '(' << p.first << ", " << p.second << ')'; }
+
+template <typename ...Args>
+ostream &operator<<(ostream &os, const tuple<Args...> &t)
+{
+  os << '{';
+  string sep;
+  apply([&](auto &&...args) { ((os << sep << args, sep = ", "), ...); }, t);
+  return os << '}';
+}
+
+template <typename T_container,
+          typename T = typename enable_if<!is_same<T_container, string>::value,
+                                                   typename T_container::value_type>::type>
+ostream &operator<<(ostream &os, const T_container &v)
+{
+  os << '{';
+  string sep;
+  for (const T &x : v)
+    os << sep << x, sep = ", ";
+  return os << '}';
+}
+
+template <typename... Args>
+void print(const Args &...args)
+{
+  bool end = true;
+  ((cout << (end ? (end = false, "") : " ") << args), ...);
+}
+
+template <typename... Args>
+void println(const Args &...args)
+{
+  bool end = true;
+  ((cout << (end ? (end = false, "") : " ") << args), ...) << "\n";
+}
+
+void dbg_out() { cerr << endl; }
+
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T)
+{
+  cerr << ' ' << H;
+  dbg_out(T...);
+}
+
+// #define LOCAL 1
+
+#ifdef LOCAL
+#define dbg(...) cerr << "(" << #__VA_ARGS__ << "):", dbg_out(__VA_ARGS__)
+#else
+#define dbg(...)
+#endif
+
+#define ar array
+#define sza(x) ((int)x.size())
+#define all(a) (a).begin(), (a).end()
+#define prec(k) cout << fixed << setprecision(k);
+#define ceildiv(n, k) (((n) + (k) - 1) / (k))
+
+using ll = long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll, ll>;
+using point = complex<double>;
+
+const int MAX_N = 1e5 + 5;
+const ll MOD = 1e9 + 7;
+const ll INF = 1e9;
+const ld EPS = 1e-9;
+
+inline ll gcd(ll a, ll b) { return b == 0 ? a : gcd(b, a % b); }
+inline ll lcm(ll a, ll b) { return a / gcd(a, b) * b; }
+
+template <typename T>
+concept Input_Streamable_Type = requires(T stdin_data)
+{
+  cin >> stdin_data;
+};
+
+template <Input_Streamable_Type IST>
+inline IST nxt()
+{
+  IST x;
+  cin >> x;
+  return x;
+}
+
+void solve()
+{
+  auto n = nxt<int>();
+  auto replace_op = nxt<int>();
+  vector<ll> a(n);
+  ranges::generate(a, nxt<ll>);
+
+  if (replace_op == n)
+  {
+    println(1);
+    return;
+  }
+
+  map<ll, int> freq;
+  for (const auto &num : a)
+    freq[num]++;
+
+  vector<pair<int, ll>> rev_freq;
+  rev_freq.reserve(sza(freq));
+  for (const auto &p : freq)
+    rev_freq.emplace_back(pair{p.second, p.first});
+  ranges::sort(rev_freq);
+
+  int ans = 0;
+  auto iter = rev_freq.begin();
+  while (replace_op && iter != rev_freq.cend())
+  {
+    if (replace_op >= iter->first)
+    {
+      replace_op -= iter->first;
+      freq[iter->second] = 0;
+      iter++;
+      continue;
+    }
+    freq[iter->second] -= replace_op;
+    break;
+  }
+
+  for (const auto &[_, f] : freq)
+    if (f)
+      ans++;
+
+  println(ans);
+}
+
+int main()
+{
+  ios_base::sync_with_stdio(0);
+  cin.exceptions(ios::badbit | ios::failbit);
+  cin.tie(0);
+  cout.tie(0);
+
+  int tc = 1;
+  cin >> tc;
+  for (int t = 1; t <= tc; t++)
+  {
+    // cout << "Case #" << t << ": "; // @Warn: Commenting before submission.
+    solve();
+  }
+}
